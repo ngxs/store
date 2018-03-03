f336cddf7322bbb60a476dc0d8afd4b94fb4723c
Danny Blue <dannybluedesign@gmail.com>
2018-03-03 15:49:22 -0500

    tweak config and add sample test

--- src/spec/action.spec.ts
+++ src/spec/action.spec.ts
@@ -1,5 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+
+import { Ngxs } from '../ngxs';
+import { NgxsModule } from '../module';
+import { Store } from '../store';
+import { Mutation } from '../mutation';
+
 describe('Action', () => {
-  it('should world', () => {
-    expect(true).toBe(true);
+  let ngxs: Ngxs;
+
+  class AddTodo {
+    constructor(public readonly payload: string) {}
+  }
+
+  @Store({
+    defaults: []
+  })
+  class TodoStore {
+    @Mutation(AddTodo)
+    addTodo(state, action: AddTodo) {
+      return [...state, action.payload];
+    }
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([TodoStore])]
+    });
+
+    ngxs = TestBed.get(Ngxs);
+  });
+
+  it('should add a todo', () => {
+    ngxs.dispatch(new AddTodo('Get Milk'));
+
+    ngxs.select(state => state.todo).subscribe(state => {
+      expect(state.length).toBe(1);
+    });
   });
 });
