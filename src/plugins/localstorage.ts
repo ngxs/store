eda37b11e468386fafde12f246109f24ea40046b
Danny Blue <dannybluedesign@gmail.com>
2018-03-08 21:41:42 -0500

    update plugins with proposed api

--- src/plugins/localstorage.ts
+++ src/plugins/localstorage.ts
@@ -1,5 +1,6 @@
+import { Injectable, NgModule, ModuleWithProviders, Inject, InjectionToken } from '@angular/core';
+
 import { NgxsPlugin } from '../symbols';
-import { Injectable } from '@angular/core';
 import { getTypeFromInstance } from '../internals';
 
 export interface LocalStoragePluginOptions {
@@ -8,52 +9,45 @@ export interface LocalStoragePluginOptions {
   deserialize(obj: any);
 }
 
-const setValue = (obj, prop, val) => {
+export const LOCAL_STORAGE_PLUGIN_OPTION = new InjectionToken('LOCAL_STORAGE_PLUGIN_OPTION');
+
+const setValue = (obj, prop: string, val) => {
   obj = { ...obj };
+
   const split = prop.split('.');
   const last = split[split.length - 1];
+
   split.reduce((acc, part) => {
     if (part === last) {
       acc[part] = val;
     } else {
       acc[part] = { ...acc[part] };
     }
+
     return acc && acc[part];
   }, obj);
+
   return obj;
 };
 
-const getValue = (obj, prop) => prop.split('.').reduce((acc, part) => acc && acc[part], obj);
+const getValue = (obj, prop: string) => prop.split('.').reduce((acc, part) => acc && acc[part], obj);
 
 @Injectable()
 export class LocalStoragePlugin implements NgxsPlugin {
-  private static _options: LocalStoragePluginOptions | undefined = undefined;
-
-  static forRoot(options) {
-    this._options = options;
-    return this;
-  }
-
-  constructor() {
-    if (!LocalStoragePlugin._options) {
-      LocalStoragePlugin._options = {
-        key: '@@STATE',
-        serialize: JSON.stringify,
-        deserialize: JSON.parse
-      };
-    }
-  }
+  constructor(@Inject(LOCAL_STORAGE_PLUGIN_OPTION) private _options: LocalStoragePluginOptions) {}
 
   handle(state, event, next) {
-    const options = LocalStoragePlugin._options || <any>{};
+    const options = this._options || <any>{};
     const isInitAction = getTypeFromInstance(event) === '@@INIT';
     const keys = Array.isArray(options.key) ? options.key : [options.key];
 
     if (isInitAction) {
       for (const key of keys) {
         let val = localStorage.getItem(key);
+
         if (val !== 'undefined' && val !== null) {
           val = options.deserialize(val);
+
           if (key !== '@@STATE') {
             state = setValue(state, key, val);
           } else {
@@ -68,13 +62,38 @@ export class LocalStoragePlugin implements NgxsPlugin {
     if (!isInitAction) {
       for (const key of keys) {
         let val = state;
+
         if (key !== '@@STATE') {
           val = getValue(state, key);
         }
+
         localStorage.setItem(key, options.serialize(val));
       }
     }
 
     return state;
+  }
+}
+
+@NgModule()
+export class LocalStoragePluginModule {
+  static forRoot(options?: LocalStoragePluginOptions): ModuleWithProviders {
+    return {
+      ngModule: LocalStoragePluginModule,
+      providers: [
+        LocalStoragePlugin,
+        {
+          provide: LOCAL_STORAGE_PLUGIN_OPTION,
+          useValue: {
+            ...{
+              key: '@@STATE',
+              serialize: JSON.stringify,
+              deserialize: JSON.parse
+            },
+            ...options
+          }
+        }
+      ]
+    };
   }
 }
