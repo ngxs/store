5e4511aec350071de486009c5af279c0088f77b6
Danny Blue <dannybluedesign@gmail.com>
2018-03-16 14:46:05 -0400

    update integration test builds and fix issue with plugins throwing ngc compiler error

--- integration/app/app.module.ts
+++ integration/app/app.module.ts
@@ -6,10 +6,18 @@ import { RouterModule } from '@angular/router';
 import { TodoState } from './todo.state';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), NgxsModule.forRoot([TodoState]), ReduxDevtoolsPluginModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    NgxsModule.forRoot([TodoState]),
+    ReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
