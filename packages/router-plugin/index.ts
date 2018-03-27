86f5dd83f71872a202cff173a2652af2b190c1f6
Leon Radley <leon@radley.se>
2018-03-27 11:07:56 +0200

    feat(plugin): router plugin (work in progress)
    A first wip of the router plugin.
    not working quite yet

---
+++ packages/router-plugin/index.ts
@@ -0,0 +1,4 @@
+export { NgxsRouterPluginModule } from './src/router.module';
+export { NgxsRouterPlugin } from './src/router.plugin';
+export { RouterState } from './src/router.state';
+export * from './src/symbols';
