6461c7836b05f34f7e744066700eb9d676b68526
Danny Blue <dannybluedesign@gmail.com>
2018-03-27 10:43:53 -0400

    refactor(store): action stream should use Subject (#178)

--- packages/store/src/actions-stream.ts
+++ packages/store/src/actions-stream.ts
@@ -1,18 +1,19 @@
 import { Injectable, Optional, SkipSelf } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
 
 /**
  * Action stream that is emitted anytime an action is dispatched.
+ *
  * You can listen to this in services to react without stores.
  */
 @Injectable()
-export class Actions extends BehaviorSubject<any> {
+export class Actions extends Subject<any> {
   constructor(
     @Optional()
     @SkipSelf()
     parent: Actions
   ) {
-    super({});
+    super();
 
     if (parent) {
       Object.assign(this, parent);
