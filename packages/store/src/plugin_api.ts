c8d9ecc49c0adc163a79b7a672b7f588f3ca6932
Austin <amcdaniel2@gmail.com>
2018-03-27 08:29:32 -0500

    chore: add types

--- packages/store/src/plugin_api.ts
+++ packages/store/src/plugin_api.ts
@@ -2,3 +2,4 @@ export { NgxsModule } from './module';
 export { NGXS_PLUGINS, NgxsPlugin } from './symbols';
 export { StateStream } from './state-stream';
 export { getActionTypeFromInstance, setValue, getValue } from './utils';
+export { InitState, UpdateState } from './actions';
