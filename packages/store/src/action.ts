09a2438be0bf729342263a0db53d698b71005a2c
Danny Blue <dannybluedesign@gmail.com>
2018-04-02 14:42:52 -0400

    fix(store): only add type if none exists

--- packages/store/src/action.ts
+++ packages/store/src/action.ts
@@ -23,9 +23,7 @@ export function Action(actions: any | any[], options?: ActionOptions) {
     }
 
     for (const action of actions) {
-      if (typeof action.type === 'string') {
-        action.type = new ActionToken(action.type);
-      } else if (!action.type) {
+      if (!action.type) {
         action.type = new ActionToken(action.name);
       }
 
