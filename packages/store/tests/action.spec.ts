47f0dee4d1da5be10c3558611ad566a21b42b79d
Danny Blue <dannybluedesign@gmail.com>
2018-04-09 21:49:07 -0400

    feat(store): update unit test to show that all cbs are called correctly

--- packages/store/tests/action.spec.ts
+++ packages/store/tests/action.spec.ts
@@ -6,55 +6,59 @@ import { META_KEY } from '../src/symbols';
 import { NgxsModule } from '../src/module';
 import { Store } from '../src/store';
 import { Actions } from '../src/actions-stream';
-import { ofActionComplete, ofActionDispatched } from '../src/of-action';
+import { ofActionComplete, ofActionDispatched, ofAction } from '../src/of-action';
 
 describe('Action', () => {
+  let store: Store;
+  let actions: Actions;
+
+  class Action1 {
+    static type = 'ACTION 1';
+  }
+
+  class Action2 {
+    static type = 'ACTION 2';
+  }
+
+  @State({
+    name: 'bar'
+  })
+  class BarStore {
+    @Action([Action1, Action2])
+    foo() {}
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([BarStore])]
+    });
+
+    store = TestBed.get(Store);
+    actions = TestBed.get(Actions);
+  });
+
   it('supports multiple actions', () => {
-    class Action1 {
-      static type = 'ACTION 1';
-    }
-
-    class Action2 {
-      static type = 'ACTION 2';
-    }
-
-    @State({
-      name: 'bar'
-    })
-    class BarStore {
-      @Action([Action1, Action2])
-      foo() {}
-    }
-
     const meta = BarStore[META_KEY];
 
     expect(meta.actions[Action1.type]).toBeDefined();
     expect(meta.actions[Action2.type]).toBeDefined();
   });
 
-  it('call action on dispatch and on complete', () => {
-    class Action1 {
-      static type = 'ACTION 1';
-    }
+  it('calls actions on dispatch and on complete', () => {
+    let callbackCalledCount = 0;
 
-    @State({ name: 'Bar' })
-    class BarStore {}
-
-    TestBed.configureTestingModule({
-      imports: [NgxsModule.forRoot([BarStore])]
+    actions.pipe(ofAction(Action1)).subscribe(action => {
+      callbackCalledCount++;
     });
 
-    const store = TestBed.get(Store);
-    const actions = TestBed.get(Actions);
-
-    let dispatchCalled = false;
-
     actions.pipe(ofActionDispatched(Action1)).subscribe(action => {
-      dispatchCalled = true;
+      callbackCalledCount++;
     });
 
     actions.pipe(ofActionComplete(Action1)).subscribe(action => {
-      expect(dispatchCalled).toBe(true);
+      callbackCalledCount++;
+
+      expect(callbackCalledCount).toBe(4);
     });
 
     store.dispatch(new Action1());
