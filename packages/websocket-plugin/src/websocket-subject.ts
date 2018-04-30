265af8a9e4f4e1c16f7ca65493436410994cb932
Austin <amcdaniel2@gmail.com>
2018-04-30 08:36:04 -0500

    fix: websocket reconnect issue

--- packages/websocket-plugin/src/websocket-subject.ts
+++ packages/websocket-plugin/src/websocket-subject.ts
@@ -1,8 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
-import { Subject, Observer, Observable, interval } from 'rxjs';
+import { Subject, Observer, Observable } from 'rxjs';
 import { WebSocketSubject as RxWebSocketSubject, WebSocketSubjectConfig } from 'rxjs/webSocket';
 import { NGXS_WEBSOCKET_OPTIONS, NgxsWebsocketPluginOptions } from './symbols';
-import { share, distinctUntilChanged, filter, takeWhile } from 'rxjs/operators';
+import { share, distinctUntilChanged } from 'rxjs/operators';
 
 /**
  * Websocket Subject
@@ -16,8 +16,6 @@ export class WebSocketSubject extends Subject<any> {
   connectionStatus: Observable<any>;
 
   private _socket: RxWebSocketSubject<any>;
-  private _reconnectionObservable: Observable<number>;
-  private _reconnectAttempts: number;
   private _connectionObserver: Observer<boolean>;
   private _internalConfig: WebSocketSubjectConfig<any>;
 
@@ -43,30 +41,33 @@ export class WebSocketSubject extends Subject<any> {
         next: (e: Event) => this._connectionObserver.next(true)
       }
     };
-
-    this.connectionStatus
-      .pipe(filter(isConnected => !this._reconnectionObservable && isConnected === false && this._socket !== undefined))
-      .subscribe(isConnected => this.reconnect());
   }
 
   /**
    * Kickoff the connection to the websocket.
    */
-  connect(url?: string) {
-    // Users can pass the URL via the setup or via
-    // the config. This is needed when the URL is totally dynamic.
-    if (url) {
-      this._internalConfig.url = url;
+  connect(options?: NgxsWebsocketPluginOptions) {
+    // Users can pass the options in the connect method so
+    // if options aren't available at DI bootstrap they have access
+    // to pass them here
+    if (options) {
+      if (options.url) {
+        this._internalConfig.url = options.url;
+      }
+
+      if (options.serializer) {
+        this._config.serializer = options.serializer;
+      }
+
+      if (options.deserializer) {
+        this._config.deserializer = options.deserializer;
+      }
     }
 
     this._socket = new RxWebSocketSubject(this._internalConfig);
     this._socket.subscribe(
-      message => this.next(message),
-      (error: Event) => {
-        if (!this._socket) {
-          this.reconnect();
-        }
-      }
+      (message: any) => this.next(message),
+      (error: Event) => console.error('Websocket error ocurred', error)
     );
   }
 
@@ -78,23 +79,6 @@ export class WebSocketSubject extends Subject<any> {
       this._socket.complete();
       this._socket = undefined;
     }
-  }
-
-  /**
-   * Try to reconnect on a interval.
-   */
-  reconnect() {
-    this._reconnectionObservable = interval(this._config.reconnectInterval).pipe(
-      takeWhile((v, index) => index < this._reconnectAttempts && !this._socket)
-    );
-
-    this._reconnectionObservable.subscribe(() => this.connect(), null, () => {
-      this._reconnectionObservable = null;
-      if (!this._socket) {
-        this.complete();
-        this._connectionObserver.complete();
-      }
-    });
   }
 
   /**
