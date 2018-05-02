927580bb60ffdbc547dd4e677e9ba8faaee38dbd
Austin <amcdaniel2@gmail.com>
2018-05-02 14:39:11 -0500

    chore: make opts optional

--- packages/logger-plugin/src/symbols.ts
+++ packages/logger-plugin/src/symbols.ts
@@ -2,13 +2,13 @@ import { InjectionToken } from '@angular/core';
 
 export interface NgxsLoggerPluginOptions {
   /** Auto expand logged actions  */
-  collapsed: boolean;
+  collapsed?: boolean;
 
   /** Provide alternate console.log implementation */
-  logger: any;
+  logger?: any;
 
   /** Disable the logger. Useful for prod mopde. */
-  disabled: boolean;
+  disabled?: boolean;
 }
 
 export const NGXS_LOGGER_PLUGIN_OPTIONS = new InjectionToken('NGXS_LOGGER_PLUGIN_OPTIONS');
