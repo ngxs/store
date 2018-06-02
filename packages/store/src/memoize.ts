97679a4fa2101d9d9e1d98b16864575d51cd8a16
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2018-06-02 23:02:25 +0200

    fix(store): memoizer function doesn't support primitive keys
    Also change to a single value cache for performance
    The deep cache isn't used anyway

--- packages/store/src/memoize.ts
+++ packages/store/src/memoize.ts
@@ -1,23 +1,40 @@
+function defaultEqualityCheck(a, b) {
+  return a === b;
+}
+
+function areArgumentsShallowlyEqual(equalityCheck, prev, next) {
+  if (prev === null || next === null || prev.length !== next.length) {
+    return false;
+  }
+
+  // Do this in a for loop (and not a `forEach` or an `every`) so we can determine equality as fast as possible.
+  const length = prev.length;
+  for (let i = 0; i < length; i++) {
+    if (!equalityCheck(prev[i], next[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 /**
- * Memoize a function.
- * Oringinally from: https://github.com/lodash/lodash/blob/master/memoize.js with some modifications
+ * Memoize a function on its last inputs only.
+ * Oringinally from: https://github.com/reduxjs/reselect/blob/master/src/index.js
  *
  * @ignore
  */
-export function memoize(func, resolver?) {
-  const memoized: any = function(...args) {
-    const key = resolver ? resolver.apply(this, args) : args[0];
-    const cache = memoized.cache;
-
-    if (cache.has(key)) {
-      return cache.get(key);
+export function memoize(func, equalityCheck = defaultEqualityCheck) {
+  let lastArgs = null;
+  let lastResult = null;
+  // we reference arguments instead of spreading them for performance reasons
+  return <(...args) => any>function memoized() {
+    if (!areArgumentsShallowlyEqual(equalityCheck, lastArgs, arguments)) {
+      // apply arguments instead of spreading for performance.
+      lastResult = func.apply(null, arguments);
     }
-    const result = func.apply(this, args);
-    memoized.cache = cache.set(key, result) || cache;
-    return result;
+
+    lastArgs = arguments;
+    return lastResult;
   };
-
-  memoized.cache = new WeakMap();
-
-  return memoized;
 }
