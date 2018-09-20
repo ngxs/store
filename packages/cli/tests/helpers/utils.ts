cfc6646d5f92f7eb7f562a95979674138f0b4bee
splincode <omaxphp@yandex.ru>
2018-09-20 16:19:08 +0300

    fix(cli): move packages from cli to root

--- packages/cli/tests/helpers/utils.ts
+++ packages/cli/tests/helpers/utils.ts
@@ -1,12 +1,16 @@
 import { CliArgvImpl, GeneratorResults } from '@ngxs/cli/tests/helpers/config';
 import * as nodePlopGenerator from '@ngxs/cli/src/node-generator';
 import { rimraf } from 'ng-packagr/lib/util/rimraf';
-import * as path from 'path';
-import * as fs from 'fs';
+
+declare const __dirname: any;
+declare const require: any;
+
+const fs = require('fs');
+const path = require('path');
 
 export async function execCli(argv: Partial<CliArgvImpl>): Promise<GeneratorResults> {
   const plopfilePath = path.resolve(__dirname, '../../plopfile.js');
-  return nodePlopGenerator({ plopfilePath, argv, showOutput: false  });
+  return nodePlopGenerator({ plopfilePath, argv, showOutput: false });
 }
 
 export function readFile(relativePath: string) {
