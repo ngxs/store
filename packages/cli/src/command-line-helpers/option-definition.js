2f40d70d712cb903bdba37740bcfda8a5e7712b6
splincode <omaxphp@yandex.ru>
2018-09-29 16:39:52 +0300

    revert(cli): remove from @ngxs/store repo

--- packages/cli/src/command-line-helpers/option-definition.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  { name: 'help', alias: 'h', type: Boolean },
-  { name: 'name', type: String },
-  { name: 'spec', type: String },
-  { name: 'directory', type: String },
-  { name: 'folder-name', type: String },
-  { name: 'plopfile', type: String }
-];
