f07d8b35a43848b055296a2b6047c834373b09b6
splincode <omaxphp@yandex.ru>
2018-10-26 16:34:46 +0300

    ci(store): set environments

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const CI = process.env['CI'] === 'true';
 
 module.exports = function(config) {
   config.set({
@@ -19,13 +20,16 @@ module.exports = function(config) {
       dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    browserConsoleLogOptions: {
+      level: CI ? 'error' : 'debug',
+      format: '%b %T: %m',
+      terminal: true
+    },
+    autoWatch: !CI,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: CI
   });
 };
