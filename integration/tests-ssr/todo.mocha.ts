494bef4eb8c7debf2fa62380f9b8d7c0fcc8fb50
Igor Kravchenko <gornivv@gmail.com>
2018-12-04 21:21:00 +0300

    test: refactor

--- integration/tests-ssr/todo.mocha.ts
+++ integration/tests-ssr/todo.mocha.ts
@@ -4,11 +4,11 @@ const request = require('./request-service');
 let body: string;
 
 describe('NGXS + SSR', () => {
-  // console.log(body);
   it('Should be exist "NgxsOnInit todo"', async () => {
     body = await request('http://localhost:4000/list');
     return expect(body.includes('NgxsOnInit todo')).to.equal(true);
   });
+
   it('Should be exist "ngOnInit todo"', async () => {
     body = await request('http://localhost:4000/list');
     expect(body.includes('ngOnInit todo')).to.equal(true);
