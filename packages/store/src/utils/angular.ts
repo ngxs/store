e98da467fc35703ed859bb425d03252662c79520
Mark Whitfeld <markwhitfeld@users.noreply.github.com>
2018-12-20 22:06:12 +0200

    fix(store): remove TestBed ref to reduce bundle size (#725)

---
+++ packages/store/src/utils/angular.ts
@@ -0,0 +1,21 @@
+import { getPlatform, COMPILER_OPTIONS, CompilerOptions, PlatformRef } from '@angular/core';
+import { memoize } from './memoize';
+
+function _isAngularInTestMode() {
+  const platformRef: PlatformRef | null = getPlatform();
+  if (!platformRef) return false;
+  const compilerOptions = platformRef.injector.get(COMPILER_OPTIONS, null);
+  if (!compilerOptions) return false;
+  const isInTestMode = compilerOptions.some((item: CompilerOptions) => {
+    const providers = (item && item.providers) || [];
+    return providers.some((provider: any) => {
+      return (
+        (provider && provider.provide && provider.provide.name === 'MockNgModuleResolver') ||
+        false
+      );
+    });
+  });
+  return isInTestMode;
+}
+
+export const isAngularInTestMode = memoize(_isAngularInTestMode);
