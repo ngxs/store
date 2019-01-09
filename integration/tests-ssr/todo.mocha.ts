6c1e1405f15cc1e1c21d3e030b48b90371cb67a1
Artur <arthurandrosovich@gmail.com>
2019-01-09 16:33:16 +0200

    feat(store): add SSR tests for lazy state

--- integration/tests-ssr/todo.mocha.ts
+++ integration/tests-ssr/todo.mocha.ts
@@ -11,9 +11,28 @@ describe('NGXS + SSR', () => {
 
   it(`"NgxsOnInit todo" + "NgxsAfterBootstrap todo" + "NgxsOnInit lazy" + "NgxsAfterBootstrap lazy" should exist`, async () => {
     body = await request('http://localhost:4000/list');
-    expect(body.includes('NgxsOnInit todo')).to.equal(true);
-    expect(body.includes('NgxsAfterBootstrap todo')).to.equal(true);
-    expect(body.includes('NgxsOnInit lazy')).to.equal(true);
-    expect(body.includes('NgxsAfterBootstrap lazy')).to.equal(true);
+
+    const ngxsOnInitIndex = body.indexOf('NgxsOnInit todo');
+    const ngxsAfterBootstrapIndex = body.indexOf('NgxsAfterBootstrap todo');
+    const ngxsOnInitLazyIndex = body.indexOf('NgxsOnInit lazy');
+    const ngxsAfterBootstrapLazyIndex = body.indexOf('NgxsAfterBootstrap lazy');
+    const stringIndexes = [
+      ngxsOnInitIndex,
+      ngxsAfterBootstrapIndex,
+      ngxsOnInitLazyIndex,
+      ngxsAfterBootstrapLazyIndex
+    ];
+
+    stringIndexes.forEach(stringIndex => {
+      expect(stringIndex).to.be.greaterThan(-1);
+    });
+
+    stringIndexes.forEach((stringIndex, index) => {
+      // If it's not the first in the array
+      // every next index should more than previous
+      if (index) {
+        expect(stringIndex).to.be.greaterThan(stringIndexes[index - 1]);
+      }
+    });
   });
 });
