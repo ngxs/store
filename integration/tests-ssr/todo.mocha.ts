85355bc5914f6d62946d83c7b71fbb14607edf96
Artur <arthurandrosovich@gmail.com>
2019-01-09 16:20:56 +0200

    feat(store): add SSR tests for lazy state

--- integration/tests-ssr/todo.mocha.ts
+++ integration/tests-ssr/todo.mocha.ts
@@ -4,18 +4,16 @@ const request = require('./request-service');
 let body: string;
 
 describe('NGXS + SSR', () => {
-  it('Should be exist "NgxsOnInit todo"', async () => {
-    body = await request('http://localhost:4000/list');
-    return expect(body.includes('NgxsOnInit todo')).to.equal(true);
-  });
-
-  it('Should be exist "ngOnInit todo"', async () => {
+  it('"ngOnInit todo" should exist', async () => {
     body = await request('http://localhost:4000/list');
     expect(body.includes('ngOnInit todo')).to.equal(true);
   });
 
-  it('"NgxsAfterBootstrap todo" should exist', async () => {
+  it(`"NgxsOnInit todo" + "NgxsAfterBootstrap todo" + "NgxsOnInit lazy" + "NgxsAfterBootstrap lazy" should exist`, async () => {
     body = await request('http://localhost:4000/list');
+    expect(body.includes('NgxsOnInit todo')).to.equal(true);
     expect(body.includes('NgxsAfterBootstrap todo')).to.equal(true);
+    expect(body.includes('NgxsOnInit lazy')).to.equal(true);
+    expect(body.includes('NgxsAfterBootstrap lazy')).to.equal(true);
   });
 });
