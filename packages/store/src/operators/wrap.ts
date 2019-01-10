99e70094606c0be3291a08970e7298fb5365a1a7
Artur <arthurandrosovich@gmail.com>
2019-01-10 22:23:34 +0200

    feat(store): add `outsideZone` option to the config

--- packages/store/src/operators/wrap.ts
+++ packages/store/src/operators/wrap.ts
@@ -1,18 +1,51 @@
 import { NgZone } from '@angular/core';
 
-import { MonoTypeOperatorFunction } from 'rxjs';
-
-import { leaveZone } from './leave-zone';
-import { enterZone } from './enter-zone';
+import {
+  MonoTypeOperatorFunction,
+  Observable,
+  Operator,
+  Subscriber,
+  Subscription
+} from 'rxjs';
 
 /**
  * Returns operator based on the provided condition `outsideZone`, that will run
  * `subscribe` inside or outside Angular's zone
  */
 export function wrap<T>(outsideZone: boolean, zone: NgZone): MonoTypeOperatorFunction<T> {
-  if (outsideZone) {
-    return leaveZone<T>(zone);
+  return (source: Observable<T>) => source.lift(new WrapOperator(outsideZone, zone));
+}
+
+class WrapOperator<T> implements Operator<T, T> {
+  constructor(private outsideZone: boolean, private zone: NgZone) {}
+
+  public call(subscriber: Subscriber<T>, source: Observable<T>): Subscription {
+    return source.subscribe(new WrapSubscriber(subscriber, this.outsideZone, this.zone));
+  }
+}
+
+class WrapSubscriber<T> extends Subscriber<T> {
+  constructor(destination: Subscriber<T>, private outsideZone: boolean, private zone: NgZone) {
+    super(destination);
   }
 
-  return enterZone<T>(zone);
+  public next(value: T): void {
+    this.invoke(() => this.destination.next!(value));
+  }
+
+  public error(error: any): void {
+    this.invoke(() => this.destination.error!(error));
+  }
+
+  public complete(): void {
+    this.invoke(() => this.destination.complete!());
+  }
+
+  private invoke(callback: (...args: any[]) => void): void {
+    if (this.outsideZone) {
+      this.zone.runOutsideAngular(callback);
+    } else {
+      this.zone.run(callback);
+    }
+  }
 }
