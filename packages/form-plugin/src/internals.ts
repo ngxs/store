4f1da2cefc9487e7d5227303abcdb41c09806e68
Artur Androsovych <arthurandrosovich@gmail.com>
2019-01-14 19:03:41 +0200

    refactor(form-plugin): remove duplicate code + add tests (#768)
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests
    
    * refactor(form-plugin): remove duplicate code + add tests

---
+++ packages/form-plugin/src/internals.ts
@@ -0,0 +1,14 @@
+export const enum AvailableControlStatus {
+  Disabled = 'disabled',
+  Dirty = 'dirty'
+}
+
+export const enum NgxsFormShapshotValueType {
+  Form = 'form',
+  Control = 'control'
+}
+
+export const enum AvailableStream {
+  ValueChanges = 'valueChanges',
+  StatusChanges = 'statusChanges'
+}
