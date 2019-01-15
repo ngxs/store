fa09517ddfcde117f255cb47a9b4435909c4fb89
Artur <arthurandrosovich@gmail.com>
2019-01-16 00:37:11 +0200

    refactor(form-plugin): structure code excluding nested ifs

--- packages/form-plugin/src/directive.ts
+++ packages/form-plugin/src/directive.ts
@@ -2,7 +2,13 @@ import { Directive, Input, OnInit, OnDestroy, ChangeDetectorRef } from '@angular
 import { FormGroupDirective } from '@angular/forms';
 import { Store, getValue } from '@ngxs/store';
 import { Subject } from 'rxjs';
-import { takeUntil, debounceTime, first } from 'rxjs/operators';
+import {
+  takeUntil,
+  debounceTime,
+  distinctUntilChanged,
+  mergeMap,
+  finalize
+} from 'rxjs/operators';
 import {
   UpdateFormStatus,
   UpdateFormValue,
@@ -15,9 +21,9 @@ import {
 export class FormDirective implements OnInit, OnDestroy {
   @Input('ngxsForm') path: string;
   @Input('ngxsFormDebounce') debounce = 100;
-  @Input('ngxsFormClearOnDestroy') clearDestroy: boolean;
+  @Input('ngxsFormClearOnDestroy') clearDestroy = false;
 
-  private _destroy$ = new Subject<null>();
+  private _destroy$ = new Subject<void>();
   private _updating = false;
 
   constructor(
@@ -31,20 +37,18 @@ export class FormDirective implements OnInit, OnDestroy {
       .select(state => getValue(state, `${this.path}.model`))
       .pipe(takeUntil(this._destroy$))
       .subscribe(model => {
-        if (!this._updating && model) {
-          this._formGroupDirective.form.patchValue(model);
-          this._cd.markForCheck();
+        if (this._updating || !model) {
+          return;
         }
+
+        this._formGroupDirective.form.patchValue(model);
+        this._cd.markForCheck();
       });
 
     // On first state change, sync form model, status and dirty with state
     this._store
-      .select(state => getValue(state, `${this.path}`))
-      .pipe(
-        takeUntil(this._destroy$),
-        first()
-      )
-      .subscribe(state => {
+      .selectOnce(state => getValue(state, `${this.path}`))
+      .subscribe(() => {
         this._store.dispatch([
           new UpdateFormValue({
             path: this.path,
@@ -65,14 +69,16 @@ export class FormDirective implements OnInit, OnDestroy {
       .select(state => getValue(state, `${this.path}.dirty`))
       .pipe(takeUntil(this._destroy$))
       .subscribe(dirty => {
-        if (this._formGroupDirective.form.dirty !== dirty) {
-          if (dirty === true) {
-            this._formGroupDirective.form.markAsDirty();
-            this._cd.markForCheck();
-          } else if (dirty === false) {
-            this._formGroupDirective.form.markAsPristine();
-            this._cd.markForCheck();
-          }
+        if (this._formGroupDirective.form.dirty === dirty || typeof dirty !== 'boolean') {
+          return;
+        }
+
+        if (dirty) {
+          this._formGroupDirective.form.markAsDirty();
+          this._cd.markForCheck();
+        } else {
+          this._formGroupDirective.form.markAsPristine();
+          this._cd.markForCheck();
         }
       });
 
@@ -80,49 +86,54 @@ export class FormDirective implements OnInit, OnDestroy {
       .select(state => getValue(state, `${this.path}.disabled`))
       .pipe(takeUntil(this._destroy$))
       .subscribe(disabled => {
-        if (this._formGroupDirective.form.disabled !== disabled) {
-          if (disabled === true) {
-            this._formGroupDirective.form.disable();
-            this._cd.markForCheck();
-          } else if (disabled === false) {
-            this._formGroupDirective.form.enable();
-            this._cd.markForCheck();
-          }
+        if (
+          this._formGroupDirective.form.disabled === disabled ||
+          typeof disabled !== 'boolean'
+        ) {
+          return;
+        }
+
+        if (disabled) {
+          this._formGroupDirective.form.disable();
+          this._cd.markForCheck();
+        } else if (disabled) {
+          this._formGroupDirective.form.enable();
+          this._cd.markForCheck();
         }
       });
 
     this._formGroupDirective
       .valueChanges!.pipe(
         debounceTime(this.debounce),
-        takeUntil(this._destroy$)
+        distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
+        takeUntil(this._destroy$),
+        mergeMap(() => {
+          const value = this._formGroupDirective.control.getRawValue();
+          this._updating = true;
+          return this._store
+            .dispatch([
+              new UpdateFormValue({
+                path: this.path,
+                value
+              }),
+              new UpdateFormDirty({
+                path: this.path,
+                dirty: this._formGroupDirective.dirty
+              }),
+              new UpdateFormErrors({
+                path: this.path,
+                errors: this._formGroupDirective.errors
+              })
+            ])
+            .pipe(finalize(() => (this._updating = false)));
+        })
       )
-      .subscribe(() => {
-        const value = this._formGroupDirective.control.getRawValue();
-        this._updating = true;
-        this._store
-          .dispatch([
-            new UpdateFormValue({
-              path: this.path,
-              value
-            }),
-            new UpdateFormDirty({
-              path: this.path,
-              dirty: this._formGroupDirective.dirty
-            }),
-            new UpdateFormErrors({
-              path: this.path,
-              errors: this._formGroupDirective.errors
-            })
-          ])
-          .subscribe({
-            error: () => (this._updating = false),
-            complete: () => (this._updating = false)
-          });
-      });
+      .subscribe();
 
     this._formGroupDirective
       .statusChanges!.pipe(
         debounceTime(this.debounce),
+        distinctUntilChanged(),
         takeUntil(this._destroy$)
       )
       .subscribe((status: string) => {
