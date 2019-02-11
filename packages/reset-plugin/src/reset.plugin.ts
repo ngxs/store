48569c7347f15794552c631cc278d35a8f522334
Levent Arman Özak <arman@armanozak.com>
2019-02-11 04:07:11 +0300

    feat(reset-plugin): add specs to suite and improve plugin to pass them

--- packages/reset-plugin/src/reset.plugin.ts
+++ packages/reset-plugin/src/reset.plugin.ts
@@ -1,9 +1,78 @@
-import { Injectable } from '@angular/core';
-import { NgxsNextPluginFn, NgxsPlugin } from '@ngxs/store';
+import { Injectable, isDevMode } from '@angular/core';
+import {
+  getActionTypeFromInstance,
+  getStoreMetadata,
+  NgxsNextPluginFn,
+  NgxsPlugin
+} from '@ngxs/store';
+import { getValue, MetaDataModel, setValue, StateClass } from './internals';
+import { StateErase, StateReset } from './symbols';
 
 @Injectable()
 export class NgxsResetPlugin implements NgxsPlugin {
+  private getErasedState(state: any, statesToKeep: MetaDataModel[]): any {
+    return statesToKeep
+      .map(meta => getPath(meta))
+      .map(path => ({
+        parts: path.split('.'),
+        value: getValue(state, path)
+      }))
+      .reduce(
+        (obj, { parts, value }) =>
+          parts.reduceRight(
+            (acc, part) =>
+              part in obj
+                ? {
+                    [part]: {
+                      ...obj[part],
+                      ...acc
+                    }
+                  }
+                : { [part]: acc },
+            value
+          ),
+        {} as any
+      );
+  }
+
+  private resetStates(state: any, statesToReset: MetaDataModel[]): any {
+    statesToReset.forEach(meta => {
+      if (meta.name && 'defaults' in meta) {
+        state = setValue(state, getPath(meta), meta.defaults);
+      } else if (isDevMode()) {
+        console.warn(`Reset Failed: ${meta.name} is not a state class.`);
+      }
+    });
+
+    return state;
+  }
+
   handle(state: any, action: any, next: NgxsNextPluginFn) {
+    const type: string = getActionTypeFromInstance(action) || '';
+
+    switch (type) {
+      case StateErase.type:
+        state = this.getErasedState(state, convertToMetaDataList(action.statesToKeep || []));
+        break;
+
+      case StateReset.type:
+        state = this.resetStates(state, convertToMetaDataList(action.statesToReset));
+        break;
+
+      default:
+        break;
+    }
+
     return next(state, action);
   }
+}
+
+function convertToMetaDataList(classes: StateClass | StateClass[]): MetaDataModel[] {
+  return (Array.isArray(classes) ? classes : [classes]).map(
+    stateClass => new Object(getStoreMetadata(stateClass)) as MetaDataModel
+  );
+}
+
+function getPath(meta: MetaDataModel): string {
+  return meta.path || '';
 }
--- packages/reset-plugin/tests/reset.plugin.spec.ts
+++ packages/reset-plugin/tests/reset.plugin.spec.ts
@@ -1,8 +1,8 @@
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Actions, NgxsModule, Store } from '@ngxs/store';
-import { NgxsResetPluginModule } from '../';
+import { NgxsResetPluginModule, StateErase, StateReset } from '../';
 import { AppState, PreferencesState, SessionState, ToDoState } from './test-states';
-import { ToDoAdd } from './test-symbols';
+import { SessionEnd, ToDoAdd } from './test-symbols';
 
 interface TestModel {
   actions$: Actions;
@@ -10,10 +10,84 @@ interface TestModel {
 }
 
 describe('NgxsResetPlugin', () => {
-  it('should be ready for testing', fakeAsync(() => {
-    setupTest();
+  it('should erase states on StateErase', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateErase());
+    tick();
+
+    expect(store.snapshot()).toEqual({});
+  }));
+
+  it('should erase states on StateErase but keep selected', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateErase(PreferencesState));
+    tick();
+
+    expect(store.snapshot()).toEqual({
+      app: { preferences: { darkmode: false, language: 'en' } }
+    });
+  }));
+
+  it('should erase states on StateErase but keep selected (multi)', fakeAsync(() => {
+    const { store } = setupTest();
+
+    const lastseen = ensureLastSeen(store);
+
+    store.dispatch(new StateErase([PreferencesState, SessionState]));
+    tick();
+
+    expect(store.snapshot()).toEqual({
+      app: { preferences: { darkmode: false, language: 'en' }, session: { lastseen } }
+    });
+  }));
+
+  it('should reset state to defaults on StateReset', fakeAsync(() => {
+    const { store } = setupTest();
+
+    store.dispatch(new StateReset(ToDoState));
+    tick();
+
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should reset state to defaults on StateReset (multi)', fakeAsync(() => {
+    const { store } = setupTest();
+
+    ensureLastSeen(store);
+
+    store.dispatch(new StateReset([SessionState, ToDoState]));
+    tick();
+
+    expect(store.selectSnapshot(SessionState)).toBeUndefined();
+    expect(store.selectSnapshot(ToDoState.list)).toEqual([]);
+  }));
+
+  it('should log a warning on StateReset with wrong payload', fakeAsync(() => {
+    const { store } = setupTest();
+    const state = store.snapshot();
+
+    console.warn = jasmine.createSpy('warning');
+
+    store.dispatch(new StateReset(ToDoAdd));
+    tick();
+
+    expect(console.warn).toHaveBeenCalled();
+    expect(store.snapshot()).toEqual(state);
   }));
 });
+
+function ensureLastSeen(store: Store): number {
+  const lastseen = new Date().valueOf();
+
+  store.dispatch(new SessionEnd(lastseen));
+  tick();
+
+  expect(store.selectSnapshot(SessionState)).toEqual({ lastseen });
+
+  return lastseen;
+}
 
 function setupTest(): TestModel {
   TestBed.configureTestingModule({
