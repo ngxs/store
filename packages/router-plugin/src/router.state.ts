8bf2a58168a3e1e87be309b32931af44f4376d69
Artur <arthurandrosovich@gmail.com>
2019-03-18 22:07:35 +0200

    fix(router-plugin): trigger navigation on the queryparams change

--- packages/router-plugin/src/router.state.ts
+++ packages/router-plugin/src/router.state.ts
@@ -5,7 +5,9 @@ import {
   Router,
   RouterStateSnapshot,
   RoutesRecognized,
-  ResolveEnd
+  ResolveEnd,
+  NavigationEnd,
+  GuardsCheckEnd
 } from '@angular/router';
 import { Action, Selector, State, StateContext, Store } from '@ngxs/store';
 
@@ -37,6 +39,7 @@ export class RouterState {
   private lastRoutesRecognized: RoutesRecognized;
   private dispatchTriggeredByRouter = false; // used only in dev mode in combination with routerReducer
   private navigationTriggeredByDispatch = false; // used only in dev mode in combination with routerReducer
+  private lastResolvedStateSnapshot: RouterStateSnapshot = null!;
 
   /**
    * Selectors
@@ -97,8 +100,11 @@ export class RouterState {
     this._router.events.subscribe(e => {
       if (e instanceof RoutesRecognized) {
         this.lastRoutesRecognized = e;
-      } else if (e instanceof ResolveEnd) {
-        this.resolveEnd(e.state);
+      } else if (e instanceof GuardsCheckEnd || e instanceof ResolveEnd) {
+        // The `GuardsCheckEnd` event is always triggered unlike the `ResolveEnd`
+        this.lastResolvedStateSnapshot = e.state;
+      } else if (e instanceof NavigationEnd) {
+        this.navigationEnd();
       } else if (e instanceof NavigationCancel) {
         this.dispatchRouterCancel(e);
       } else if (e instanceof NavigationError) {
@@ -107,12 +113,8 @@ export class RouterState {
     });
   }
 
-  /**
-   * The `ResolveEnd` event is always triggered after running all resolvers
-   * that are linked to some route and child routes
-   */
-  private resolveEnd(routerStateSnapshot: RouterStateSnapshot): void {
-    this.routerStateSnapshot = this._serializer.serialize(routerStateSnapshot);
+  private navigationEnd(): void {
+    this.routerStateSnapshot = this._serializer.serialize(this.lastResolvedStateSnapshot);
     if (this.shouldDispatchRouterNavigation()) {
       this.dispatchRouterNavigation();
     }
@@ -124,10 +126,9 @@ export class RouterState {
   }
 
   private navigateIfNeeded(): void {
-    if (!this.routerState || !this.routerState.state) {
+    if (!this.routerState || !this.routerState.state || this.dispatchTriggeredByRouter) {
       return;
     }
-    if (this.dispatchTriggeredByRouter) return;
 
     if (this._router.url !== this.routerState.state.url) {
       this.navigationTriggeredByDispatch = true;
