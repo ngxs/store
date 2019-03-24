f9a2948797501a6c0164761a4d218d15b0d8433c
splincode <omaxphp@yandex.ru>
2019-03-24 21:09:43 +0300

    chore: update karma-cli

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 const CI = process.env['CI'] === 'true';
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
@@ -17,23 +14,18 @@ module.exports = function(config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    client: { clearContext: false },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    angularCli: {
-      environment: 'production'
-    },
+    angularCli: { environment: 'production' },
     reporters: ['jasmine-diff', 'mocha', 'kjhtml'],
-    mochaReporter: {
-      output: 'minimal'
-    },
+    mochaReporter: { output: 'minimal' },
     port: 9876,
     colors: true,
+    failOnEmptyTestSuite: false,
     logLevel: config.LOG_INFO,
     browserConsoleLogOptions: {
       level: CI ? 'error' : 'debug',
