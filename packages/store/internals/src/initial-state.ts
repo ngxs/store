b195dd90b564b9dc0d93068ebe7fb8c2ab93d5f1
markwhitfeld <markwhitfeld@users.noreply.github.com>
2019-05-16 09:55:45 +0200

    chore: use ObjectKeyMap for the InitialState class value

--- packages/store/internals/src/initial-state.ts
+++ packages/store/internals/src/initial-state.ts
@@ -1,16 +1,17 @@
 import { InjectionToken } from '@angular/core';
+import { ObjectKeyMap } from './symbols';
 
 export const INITIAL_STATE_TOKEN = new InjectionToken<any>('INITIAL_STATE_TOKEN');
 
 export class InitialState {
-  private static value = {};
+  private static value: ObjectKeyMap<any> = {};
 
-  public static set(state: any) {
+  public static set(state: ObjectKeyMap<any>) {
     this.value = state;
   }
 
   public static pop() {
-    const state = this.value;
+    const state: ObjectKeyMap<any> = this.value;
     this.value = {};
     return state;
   }
