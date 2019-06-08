f0614d3eefc1609b5d21a3f411fe4a974e1c771c
David Herberth <github@dav1d.de>
2019-06-08 22:45:40 +0200

    fix(storage-plugin): fix undefined localStorage error during SSR (#1119)

--- integration/app/app.server.module.ts
+++ integration/app/app.server.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppComponent } from '@integration/app.component';
 import { AppModule } from '@integration/app.module';
+import { TODOS_STORAGE_KEY } from '@integration/store/todos/todos.model';
 
 @NgModule({
   imports: [
@@ -12,7 +14,8 @@ import { AppModule } from '@integration/app.module';
     ServerModule,
     NoopAnimationsModule,
     ServerTransferStateModule,
-    ModuleMapLoaderModule
+    ModuleMapLoaderModule,
+    NgxsStoragePluginModule.forRoot({ key: [TODOS_STORAGE_KEY] })
   ],
   bootstrap: [AppComponent]
 })
