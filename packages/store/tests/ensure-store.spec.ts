a30cac29d8a7b446626b1d1c6ceaace0d40be308
splincode <omaxphp@yandex.ru>
2019-08-24 01:55:05 +0300

    test(store): ensure store for plugins

--- packages/store/tests/ensure-store.spec.ts
+++ packages/store/tests/ensure-store.spec.ts
@@ -1,36 +1,214 @@
+import {
+  State,
+  Action,
+  getStoreMetadata,
+  getSelectorMetadata,
+  Selector,
+  NgxsModule,
+  SelectorOptions
+} from '@ngxs/store';
 import { TestBed } from '@angular/core/testing';
-import { State, NgxsModule, ensureStoreMetadata, Action } from '@ngxs/store';
 
-describe('Ensure store for plugins', () => {
-  @State({ name: 'count', defaults: 0 })
-  class CountState {
-    @Action({ type: 'increment' })
-    public addOne(): void {}
+import { SelectorMetaDataModel } from '../src/internal/internals';
+import { getSelectorFn } from '../src/utils/selector-utils';
 
-    @Action({ type: 'increment' })
-    public addTwo(): void {}
-  }
-
-  beforeAll(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NgxsModule.forRoot([CountState])]
-    }).compileComponents();
+describe('Ensure metadata', () => {
+  it('should return undefined if not a state class', () => {
+    class MyState {}
+    expect(getStoreMetadata(MyState)).toBeUndefined();
+    expect(getSelectorMetadata(MyState)).toBeUndefined();
   });
 
-  it('should be get meta data from state', () => {
-    expect(ensureStoreMetadata(CountState)).toEqual({
+  describe('Ensure store for plugins', () => {
+    @State({
+      name: 'myCounter',
+      defaults: 1
+    })
+    class MyCounterState {
+      @Selector()
+      @SelectorOptions({ suppressErrors: false })
+      public static canInheritSelectFn(state: number): number {
+        return state * 2;
+      }
+
+      @Action({ type: 'decrement' })
+      public decrement(): void {}
+    }
+
+    @State({
       name: 'count',
-      actions: {
-        increment: [
-          { fn: 'addOne', options: {}, type: 'increment' },
-          { fn: 'addTwo', options: {}, type: 'increment' }
-        ]
-      },
       defaults: 0,
-      path: null,
-      selectFromAppState: null,
-      children: undefined,
-      instance: null
+      children: [MyCounterState]
+    })
+    class CountState extends MyCounterState {
+      @Selector()
+      public static selectFn(state: number): number {
+        return state;
+      }
+
+      @Action({ type: 'increment' })
+      public addOne(): void {}
+
+      @Action({ type: 'increment' })
+      public addTwo(): void {}
+    }
+
+    beforeAll(() => {
+      TestBed.configureTestingModule({
+        imports: [NgxsModule.forRoot([CountState, MyCounterState])]
+      });
     });
+
+    it('should get the meta data from the CountState', () => {
+      expect(getStoreMetadata(CountState)).toEqual({
+        name: 'count',
+        actions: {
+          increment: [
+            { fn: 'addOne', options: {}, type: 'increment' },
+            { fn: 'addTwo', options: {}, type: 'increment' }
+          ],
+          decrement: [{ fn: 'decrement', options: {}, type: 'decrement' }]
+        },
+        defaults: 0,
+        path: null,
+        instance: null,
+        selectFromAppState: null,
+        children: [MyCounterState]
+      });
+    });
+
+    it('should get the meta data from the MyCounterState', () => {
+      expect(getStoreMetadata(MyCounterState)).toEqual({
+        name: 'myCounter',
+        actions: { decrement: [{ fn: 'decrement', options: {}, type: 'decrement' }] },
+        defaults: 1,
+        path: null,
+        selectFromAppState: null,
+        children: undefined,
+        instance: null
+      });
+    });
+
+    it('should get the selector meta data from the CountState, MyCounterState', () => {
+      expect(getSelectorMetadata(CountState)).toBeUndefined();
+      expect(getSelectorMetadata(MyCounterState)).toBeUndefined();
+    });
+
+    it('should get the selector meta data from the CountState.selectFn', () => {
+      const metadata: SelectorMetaDataModel = getSelectorMetadata(CountState.selectFn);
+
+      expect(metadata.selectorName).toEqual('selectFn');
+      expect(metadata.containerClass).toEqual(CountState);
+
+      expect(metadata.originalFn).not.toEqual(CountState.selectFn);
+      expect(extractFunctionBody(metadata.originalFn)).toEqual(
+        'function (state) { return state; }'
+      );
+
+      expect(metadata.originalFn!(1)).toEqual(1); // state => state
+
+      expect(extractFunctionBody(metadata.getSelectorOptions)).toEqual(
+        'function () { return getCustomSelectorOptions(selectorMetaDataClone, getExplicitSelectorOptions()); }'
+      );
+
+      expect(metadata.getSelectorOptions()).toEqual({});
+      expect(metadata.selectFromAppState).toEqual(getSelectorFn(CountState.selectFn));
+
+      // TODO(splincode): is normal for CountState?
+      expect(getSelectorFn(CountState.selectFn)(0)).toBeUndefined();
+    });
+
+    it('should get the selector meta data from the CountState.canInheritSelectFn, MyCounterState.canInheritSelectFn', () => {
+      const countMetadata: SelectorMetaDataModel = getSelectorMetadata(
+        CountState.canInheritSelectFn
+      );
+
+      const myCounterMetadata: SelectorMetaDataModel = getSelectorMetadata(
+        MyCounterState.canInheritSelectFn
+      );
+
+      expect(countMetadata.selectorName).toEqual('canInheritSelectFn');
+      expect(myCounterMetadata.selectorName).toEqual('canInheritSelectFn');
+
+      // TODO(splincode): is normal for CountState?
+      expect(countMetadata.containerClass).toEqual(MyCounterState);
+      expect(myCounterMetadata.containerClass).toEqual(MyCounterState);
+
+      expect(countMetadata.originalFn).not.toEqual(CountState.canInheritSelectFn);
+      expect(myCounterMetadata.originalFn).not.toEqual(MyCounterState.canInheritSelectFn);
+
+      expect(extractFunctionBody(countMetadata.originalFn)).toEqual(
+        'function (state) { return state * 2; }'
+      );
+
+      expect(extractFunctionBody(myCounterMetadata.originalFn)).toEqual(
+        'function (state) { return state * 2; }'
+      );
+
+      expect(countMetadata.originalFn!(1)).toEqual(2); // state => state * 2
+      expect(myCounterMetadata.originalFn!(1)).toEqual(2); // state => state * 2
+
+      expect(extractFunctionBody(countMetadata.getSelectorOptions)).toEqual(
+        'function () { return getCustomSelectorOptions(selectorMetaDataClone, getExplicitSelectorOptions()); }'
+      );
+
+      expect(extractFunctionBody(myCounterMetadata.getSelectorOptions)).toEqual(
+        'function () { return getCustomSelectorOptions(selectorMetaDataClone, getExplicitSelectorOptions()); }'
+      );
+
+      expect(countMetadata.getSelectorOptions()).toEqual({ suppressErrors: false });
+      expect(myCounterMetadata.getSelectorOptions()).toEqual({ suppressErrors: false });
+
+      expect(countMetadata.selectFromAppState).toEqual(
+        getSelectorFn(CountState.canInheritSelectFn)
+      );
+
+      expect(myCounterMetadata.selectFromAppState).toEqual(
+        getSelectorFn(MyCounterState.canInheritSelectFn)
+      );
+
+      // TODO(splincode): is normal for CountState, MyCounterState?
+      expect(getSelectorFn(CountState.canInheritSelectFn)(0)).toEqual(NaN);
+      expect(getSelectorFn(MyCounterState.canInheritSelectFn)(0)).toEqual(NaN);
+    });
+
+    it('should get the selector meta data from the SuperCountState.canInheritSelectFn', () => {
+      try {
+        @State({
+          name: 'superCount',
+          defaults: 0
+        })
+        class SuperCountState extends MyCounterState {
+          @Selector()
+          public static canInheritSelectFn(state: number): number {
+            return super.canInheritSelectFn(state) + 1;
+          }
+        }
+
+        const metadata: SelectorMetaDataModel = getSelectorMetadata(
+          SuperCountState.canInheritSelectFn
+        );
+
+        expect(metadata.containerClass).toEqual(SuperCountState);
+      } catch (e) {
+        // TODO(splincode): is normal for SuperCountState?
+        expect(extractFunctionBody(e.message)).toEqual(
+          'Cannot set property canInheritSelectFn of function MyCounterState() { } which has only a getter'
+        );
+      }
+    });
+
+    /**
+     * @internal
+     * One of the problems to expect identical in the jest test <expect.any(Function)>
+     * of the function used in runtime is to take its compiled function body
+     * @param fn - any anonymous function
+     */
+    function extractFunctionBody(fn: Function | null | string): string {
+      return fn!
+        .toString()
+        .replace(/\n/g, '')
+        .replace(/\s\s+/g, ' ');
+    }
   });
 });
