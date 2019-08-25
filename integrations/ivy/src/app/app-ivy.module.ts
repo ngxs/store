755ab36e1dbdc5cffec8fb2fa9ab26bcadb04e88
splincode <omaxphp@yandex.ru>
2019-08-25 22:37:10 +0300

    small fix

--- integrations/ivy/src/app/app-ivy.module.ts
+++ integrations/ivy/src/app/app-ivy.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreIvyModule } from './store-ivy.module';
 
-const routes: Routes = [{ path: '', component: AppComponent }];
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), StoreIvyModule],
+  imports: [
+    BrowserModule,
+    StoreIvyModule,
+    RouterModule.forRoot([{ path: '', component: AppComponent }])
+  ],
   bootstrap: [AppComponent]
 })
 export class AppIvyModule {}
