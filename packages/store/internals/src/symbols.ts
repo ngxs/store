e597505c74a114b275a6562b101b962410b90cdf
splincode <omaxphp@yandex.ru>
2019-08-26 21:56:49 +0300

    small fix

--- packages/store/internals/src/symbols.ts
+++ packages/store/internals/src/symbols.ts
@@ -1,18 +1,9 @@
-/**
- * @internal
- */
 export interface PlainObject {
   [key: string]: any;
 }
 
-/**
- * @internal
- */
 export interface PlainObjectOf<T> {
   [key: string]: T;
 }
 
-/**
- * @internal
- */
 export type StateClass<T = any> = new (...args: any[]) => T;
