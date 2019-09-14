8f1092670f0d177e2e39922e5ae648657a90a197
splincode <omaxphp@yandex.ru>
2019-09-14 11:10:51 +0300

    feat(store): expose StateContextFactory, StateFactory
    For the plugin `@ngxs-labs/data` needs a more api from `@ngxs/store`.
    Because `@ngxs-labs/data` use deep internal manipulation with Store.

--- packages/store/src/plugin_api.ts
+++ packages/store/src/plugin_api.ts
@@ -1,5 +1,10 @@
+/**
+ * TODO(splincode): move to `@ngxs/store/plugin-api` since v4
+ */
 export { NgxsModule } from './module';
 export { NGXS_PLUGINS, NgxsPlugin, NgxsPluginFn, NgxsNextPluginFn } from './symbols';
 export { StateStream } from './internal/state-stream';
 export { getActionTypeFromInstance, setValue, getValue } from './utils/utils';
 export { InitState, UpdateState } from './actions/actions';
+export { StateContextFactory } from './internal/state-context-factory';
+export { StateFactory } from './internal/state-factory';
