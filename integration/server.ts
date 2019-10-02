5898acd4fc19d1b210fa92f83d8a67e1456325e1
Artur Androsovych <arthurandrosovich@gmail.com>
2019-10-02 23:08:40 +0300

    build: run SSR tests with cypress (#1281)
    * build: run SSR tests with cypress
    
    * fix: patch to spec json
    
    * fix: restore cache for cypress
    
    * feat: add "e2e:ssr" script
    
    * update

--- integration/server.ts
+++ integration/server.ts
@@ -1,92 +1,48 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
-const fs = require('fs');
-const path = require('path');
-const files: string[] = fs.readdirSync(`./../dist-integration-server`);
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import * as express from 'express';
 
 import { enableProdMode } from '@angular/core';
-import * as express from 'express';
-const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+import { renderModuleFactory } from '@angular/platform-server';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-const mainFiles = files.filter(file => file.startsWith('main'));
-const hash = mainFiles[0].split('.')[1];
 const {
   AppServerModuleNgFactory,
   LAZY_MODULE_MAP
-} = require(`./../dist-integration-server/main.${hash}`);
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import { exit } from 'process';
-const PORT = process.env.PORT || 4000;
+} = require('../dist-integration-server/main');
+
+const PORT = process.env.PORT || 4200;
+const DIST_FOLDER = join(__dirname, '../dist-integration');
 
 enableProdMode();
 
 const app = express();
-app.use((req, res, next) => {
-  console.log(req.url);
-  if (req.url === '/robots.txt') {
-    return;
-  }
+// Read `index.html` only once and cache it
+const document = readFileSync(join(DIST_FOLDER, 'index.html')).toString();
 
-  if (req.url === '/integration/favicon.ico') {
-    return;
-  }
+// `index: false` means to ignore plain `index.html` thus
+// the render responsibility is fully taken by Angular Universal
+app.use(express.static(DIST_FOLDER, { index: false }));
 
-  if (req.url === '/test/exit') {
-    res.send('exit');
-    exit(0);
-    return;
-  }
-  next();
-});
-
-type NgExpressEngines = (
-  path: string,
-  options: object,
-  callback: (e: any, rendered: string) => void
-) => void;
-
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [provideModuleMap(LAZY_MODULE_MAP)]
-}) as NgExpressEngines);
-
-app.set('view engine', 'html');
-app.set('views', '.');
-
-app.get('*.*', express.static(path.join(__dirname, '..', 'dist-integration')));
-
-app.get('*', (req, res) => {
-  const http =
-    req.headers['x-forwarded-proto'] === undefined ? 'http' : req.headers['x-forwarded-proto'];
-
-  const url = req.originalUrl;
+app.get('*', async (req, res) => {
+  const url = req.url;
   // tslint:disable-next-line:no-console
   console.time(`GET: ${url}`);
-  res.render(
-    '../dist-integration/index',
-    {
-      req: req,
-      res: res,
-      providers: [
-        {
-          provide: 'ORIGIN_URL',
-          useValue: `${http}://${req.headers.host}`
-        }
-      ]
-    },
-    (err, html) => {
-      if (!!err) {
-        throw err;
-      }
 
-      // tslint:disable-next-line:no-console
-      console.timeEnd(`GET: ${url}`);
-      res.send(html);
-    }
-  );
+  const html = await renderModuleFactory(AppServerModuleNgFactory, {
+    url,
+    document,
+    extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
+  });
+
+  // tslint:disable-next-line:no-console
+  console.timeEnd(`GET: ${url}`);
+  res.send(html);
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}!`);
+  console.log(`Express server is running and listening at http://localhost:${PORT}!`);
 });
