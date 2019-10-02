071de584142dfd2f642f782fb90ad505c69d9ed1
splincode <omaxphp@yandex.ru>
2019-10-02 20:53:32 +0300

    small fix

--- packages/store/internals/src/internal-tokens.ts
+++ packages/store/internals/src/internal-tokens.ts
@@ -3,13 +3,13 @@ import { InjectionToken } from '@angular/core';
 /**
  * @see StateContextFactory as it's referenced by this token to be accessed by plugins internally
  */
-export const NGXS_STATE_CONTEXT_FACTORY: InjectionToken<unknown> = new InjectionToken(
+export const NGXS_STATE_CONTEXT_FACTORY: InjectionToken<any> = new InjectionToken(
   'Internals.StateContextFactory'
 );
 
 /**
  * @see StateFactory as it's referenced by this token to be accessed by plugins internally
  */
-export const NGXS_STATE_FACTORY: InjectionToken<unknown> = new InjectionToken(
+export const NGXS_STATE_FACTORY: InjectionToken<any> = new InjectionToken(
   'Internals.StateFactory'
 );
