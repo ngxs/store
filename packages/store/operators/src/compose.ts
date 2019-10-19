710e60354799f6fdf1baef61819d6925d38f5f9f
splincode <omaxphp@yandex.ru>
2019-10-19 19:14:20 +0300

    fix(store): explicit typings for state operators

--- packages/store/operators/src/compose.ts
+++ packages/store/operators/src/compose.ts
@@ -1,6 +1,6 @@
 import { StateOperator } from '@ngxs/store';
 
-export function compose<T>(...operators: StateOperator<T>[]) {
+export function compose<T>(...operators: StateOperator<T>[]): StateOperator<T> {
   return function composeOperator(existing: Readonly<T>): T {
     return operators.reduce((accumulator, operator) => operator(accumulator), existing);
   };
