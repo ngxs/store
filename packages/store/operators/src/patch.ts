f15fdd38cb2d68bab58e80798316c2f967a40c1b
splincode <omaxphp@yandex.ru>
2019-10-23 23:15:46 +0300

    small fix

--- packages/store/operators/src/patch.ts
+++ packages/store/operators/src/patch.ts
@@ -1,5 +1,13 @@
+import { StateOperator } from '@ngxs/store';
 import { isStateOperator } from './utils';
-import { PatchOperator, PatchSpec, PatchValues } from './internals';
+
+export type PatchSpec<T> = { [P in keyof T]?: T[P] | StateOperator<NonNullable<T[P]>> };
+
+type PatchValues<T> = {
+  readonly [P in keyof T]?: T[P] extends (...args: any[]) => infer R ? R : T[P];
+};
+
+type PatchOperator<T> = <U extends PatchValues<T>>(existing: Readonly<U>) => U;
 
 export function patch<T>(patchObject: PatchSpec<T>): PatchOperator<T> {
   return function patchStateOperator<U extends PatchValues<T>>(existing: Readonly<U>): U {
