c2ce75b526551057a548d0297c9def7ba68471d4
markwhitfeld <markwhitfeld@users.noreply.github.com>
2019-11-09 11:19:01 +0200

    chore: remove all StateToken singleton and service locator code

--- packages/store/src/state-token/state-token.ts
+++ packages/store/src/state-token/state-token.ts
@@ -1,42 +1,10 @@
-import { StateClassToken, StateTokenStructure, TokenName } from './symbols';
-import { getStoreMetadata, MetaDataModel, StateClassInternal } from '../internal/internals';
+import { TokenName } from './symbols';
 
 export class StateToken<T> {
-  private static tokenMap: Map<string, StateTokenStructure> = new Map();
-  private _stateClass: Readonly<StateClassInternal>;
-
-  public set stateClass(stateClass: Readonly<StateClassInternal>) {
-    this._stateClass = stateClass;
-  }
-
-  public get stateClass(): Readonly<StateClassInternal> {
-    return this._stateClass;
-  }
-
   protected constructor(public readonly name: string) {}
 
   public static create<U = void>(name: TokenName<U>): StateToken<U> {
-    let tokenInstance: StateToken<U> | null = null;
-
-    if (this.tokenMap.has(name)) {
-      throw new Error(`StateToken ${name} is already initialized`);
-    } else {
-      tokenInstance = new StateToken<U>(name);
-      StateToken.tokenMap.set(name, { name, tokenInstance });
-    }
-
-    return tokenInstance!;
-  }
-
-  public static inject<U = void>(token: TokenName<U> | StateClassToken<U>): StateToken<U> {
-    const meta: MetaDataModel | null = getStoreMetadata(token as any) || null;
-    const tokenName: string | null = meta ? meta.name : (token as string),
-      structure: StateTokenStructure | undefined = StateToken.tokenMap.get(tokenName!);
-    return structure ? structure.tokenInstance : null!;
-  }
-
-  public static clear(): void {
-    StateToken.tokenMap.clear();
+    return new StateToken<U>(name);
   }
 
   public select(state: T): (state: T) => T {
--- packages/store/src/state-token/symbols.ts
+++ packages/store/src/state-token/symbols.ts
@@ -1,14 +1,6 @@
 import { StateToken } from './state-token';
-import { StateClassInternal } from '../internal/internals';
 
 type RequireGeneric<T, U> = T extends void ? 'You must provide a type parameter' : U;
-
-export interface StateTokenStructure {
-  name: string;
-  tokenInstance: StateToken<any>;
-}
-
-export type StateClassToken<T> = StateClassInternal & RequireGeneric<T, StateClassInternal>;
 
 export type TokenName<T> = string & RequireGeneric<T, string>;
 
