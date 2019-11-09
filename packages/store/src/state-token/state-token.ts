a5da79927f883c21832098b7b795aec4622b6178
markwhitfeld <markwhitfeld@users.noreply.github.com>
2019-11-09 11:44:11 +0200

    chore: enhance StateToken to behave as a valid selector

--- packages/store/src/state-token/state-token.ts
+++ packages/store/src/state-token/state-token.ts
@@ -1,7 +1,18 @@
 import { TokenName } from './symbols';
+import { ensureSelectorMetadata, propGetter } from '../internal/internals';
+import { SelectFactory } from '../decorators/select/select-factory';
 
 export class StateToken<T> {
-  protected constructor(private readonly name: TokenName<T>) {}
+  protected constructor(private readonly name: TokenName<T>) {
+    const selectorMetadata = ensureSelectorMetadata(<any>this);
+    selectorMetadata.selectFromAppState = (state: any) => {
+      // This is lazy initialized with the select from app state function
+      // so that it can get the config at the last responsible moment
+      const getter = propGetter([this.name], SelectFactory.config!);
+      selectorMetadata.selectFromAppState = getter;
+      return getter(state);
+    };
+  }
 
   public static create<U = void>(name: TokenName<U>): StateToken<U> {
     return new StateToken<U>(name);
