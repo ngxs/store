778460a29b63c3f5c9bbf81e061b47bef65c988e
splincode <omaxphp@yandex.ru>
2019-11-25 23:06:14 +0300

    breaking: remove ObjectUtils

--- packages/store/internals/src/object-utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated will be removed after v4
- */
-export namespace ObjectUtils {
-  export function merge(a: object, b: object): object {
-    return { ...(a || {}), ...(b || {}) };
-  }
-}
