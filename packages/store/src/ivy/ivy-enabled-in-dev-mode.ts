42aac0a630c42cf5bb033dc28983decbf08db383
arturovt <arthurandrosovich@gmail.com>
2019-12-02 00:30:30 +0200

    fix: rename

--- packages/store/src/ivy/ivy-enabled-in-dev-mode.ts
+++ packages/store/src/ivy/ivy-enabled-in-dev-mode.ts
@@ -1,4 +1,4 @@
-let ivyEnabledInDevMode: boolean | null = null;
+let _ivyEnabledInDevMode: boolean | null = null;
 
 /**
  * Ivy exposes helper functions to the global `window.ng` object.
@@ -6,13 +6,14 @@ let ivyEnabledInDevMode: boolean | null = null;
  * getListeners, getViewComponent, getHostElement, getInjector,
  * getRootComponents, getDirectives, getDebugNode, markDirty`
  * Previously, old view engine exposed `window.ng.coreTokens` and
- * `window.ng.probe` in development/production modes. Ivy doesn't
- * expose these functions in production mode. Developers will be able
- * to see warnings in both JIT/AOT modes, but only in development mode.
+ * `window.ng.probe` if an application was in development/production.
+ * Ivy doesn't expose these functions in production. Developers will be able
+ * to see warnings in both JIT/AOT modes, but only if an application
+ * is in development.
  */
-export async function ivyEnabledInDevMode$(): Promise<boolean> {
-  if (ivyEnabledInDevMode !== null) {
-    return ivyEnabledInDevMode;
+export async function ivyEnabledInDevMode(): Promise<boolean> {
+  if (_ivyEnabledInDevMode !== null) {
+    return _ivyEnabledInDevMode;
   }
 
   // Unfortunately, NGXS code is running before Ivy exposes those helper functions
@@ -23,7 +24,7 @@ export async function ivyEnabledInDevMode$(): Promise<boolean> {
     // `try-catch` will also handle server-side rendering, as
     // `window is not defined` will not be thrown.
     const ng = (window as any).ng;
-    return (ivyEnabledInDevMode =
+    return (_ivyEnabledInDevMode =
       !!ng && typeof ng.getComponent === 'function' && typeof ng.markDirty === 'function');
   } catch {
     return false;
