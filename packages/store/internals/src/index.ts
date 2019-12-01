dd1fcaf93f95cce232c5d980883b33fb8a1ea005
Max Ivanov <omaxphp@yandex.ru>
2019-12-02 00:20:27 +0300

    refactor(store): remove unneeded mergeObject function (#1463)
    * fix(store): reduce bundle size
    
    * breaking: remove ObjectUtils
    
    * small fix
    
    * small fix

--- packages/store/internals/src/index.ts
+++ packages/store/internals/src/index.ts
@@ -1,7 +1,6 @@
 export { isAngularInTestMode } from './angular';
 export { NgxsBootstrapper } from './ngxs-bootstrapper';
 export { memoize } from './memoize';
-export { ObjectUtils } from './object-utils';
 export { INITIAL_STATE_TOKEN, InitialState } from './initial-state';
 export { PlainObjectOf, PlainObject, StateClass } from './symbols';
 export { NGXS_STATE_CONTEXT_FACTORY, NGXS_STATE_FACTORY } from './internal-tokens';
--- packages/store/src/store.ts
+++ packages/store/src/store.ts
@@ -2,7 +2,7 @@
 import { Inject, Injectable, Optional, Type } from '@angular/core';
 import { Observable, of, Subscription, throwError } from 'rxjs';
 import { catchError, distinctUntilChanged, map, take } from 'rxjs/operators';
-import { INITIAL_STATE_TOKEN, ObjectUtils, PlainObject } from '@ngxs/store/internals';
+import { INITIAL_STATE_TOKEN, PlainObject } from '@ngxs/store/internals';
 
 import { InternalNgxsExecutionStrategy } from './execution/internal-ngxs-execution-strategy';
 import { InternalStateOperations } from './internal/state-operations';
@@ -109,7 +109,7 @@ export class Store {
     if (storeIsEmpty) {
       const defaultStateNotEmpty: boolean = Object.keys(this._config.defaultsState).length > 0;
       const storeValues: PlainObject = defaultStateNotEmpty
-        ? ObjectUtils.merge(this._config.defaultsState, initialStateValue)
+        ? { ...this._config.defaultsState, ...initialStateValue }
         : initialStateValue;
 
       this._stateStream.next(storeValues);
