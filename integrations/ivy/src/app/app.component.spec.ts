41dc4a89fbe64ea709c5c4d8dc637676f69cba2a
arturovt <arthurandrosovich@gmail.com>
2019-12-03 20:50:39 +0200

    test: add Ivy test

--- integrations/ivy/src/app/app.component.spec.ts
+++ integrations/ivy/src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { ɵivyEnabled as ivyEnabled } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { State, NgxsModule } from '@ngxs/store';
 
 describe('NGXS with Ivy enabled', () => {
   it('ivy has to be enabled', () => {
@@ -7,5 +9,26 @@ describe('NGXS with Ivy enabled', () => {
     expect(ivyEnabled).toBeTruthy();
   });
 
-  // here we can write next tests for Ivy
+  it('should warn if state class is not decorated with @Injectable()', () => {
+    // Arrange
+    const spy = jest.spyOn(console, 'warn');
+
+    @State({
+      name: 'countries'
+    })
+    class CountriesState {}
+
+    // Act
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([CountriesState])]
+    });
+
+    TestBed.inject(CountriesState);
+
+    // Assert
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(
+      `'CountriesState' class should be decorated with @Injectable() right after the @State() decorator`
+    );
+  });
 });
