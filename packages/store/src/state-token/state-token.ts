959b9a707d4e98bd273762198f1d92fbd6dfb5df
markwhitfeld <markwhitfeld@users.noreply.github.com>
2019-12-31 14:31:56 +0200

    fix: add support for using state tokens in sub states

--- packages/store/src/state-token/state-token.ts
+++ packages/store/src/state-token/state-token.ts
@@ -1,16 +1,14 @@
 import { TokenName } from './symbols';
-import { ensureSelectorMetadata, propGetter } from '../internal/internals';
-import { SelectFactory } from '../decorators/select/select-factory';
+import { ensureSelectorMetadata, RuntimeSelectorContext } from '../internal/internals';
 
 export class StateToken<T = void> {
   constructor(private readonly name: TokenName<T>) {
     const selectorMetadata = ensureSelectorMetadata(<any>this);
-    selectorMetadata.selectFromAppState = (state: any): T => {
-      // This is lazy initialized with the select from app state function
-      // so that it can get the config at the last responsible moment
-      const getter = propGetter([this.name], SelectFactory.config!);
-      selectorMetadata.selectFromAppState = getter;
-      return getter(state);
+    selectorMetadata.selectFromAppState = (
+      state: any,
+      runtimeContext: RuntimeSelectorContext
+    ): T => {
+      return runtimeContext.getStateGetter(this.name)(state);
     };
   }
 
