e2b0d6eaeb9fdfe3b762a78236a5becd1b56238f
Arman Ozak <arman@armanozak.com>
2020-04-24 11:48:39 +0300

    feat(logger-plugin): add state as predicate parameter

--- packages/logger-plugin/src/symbols.ts
+++ packages/logger-plugin/src/symbols.ts
@@ -10,8 +10,8 @@ export interface NgxsLoggerPluginOptions {
   /** Disable the logger. Useful for prod mode. */
   disabled?: boolean;
 
-  /** Predicate for actions to be the logged. */
-  filter?: (action: any) => boolean;
+  /** Predicate for actions to be the logged. Takes action and state snapshot as parameters */
+  filter?: (action: any, state: any) => boolean;
 }
 
 export const NGXS_LOGGER_PLUGIN_OPTIONS = new InjectionToken('NGXS_LOGGER_PLUGIN_OPTIONS');
