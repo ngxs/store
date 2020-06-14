69beaeed2c8832d2b502f998abf5a24e9ffadec4
Artur Androsovych <arthurandrosovich@gmail.com>
2020-06-14 08:25:26 +0300

    fix: do not cancel actions if action handler completes without emitting (#1615)
    * fix: do not cancel actions if action handler completes without emitting
    
    * chore: update CHANGELOG.md

---
+++ packages/store/tests/issues/issue-1568-return-empty.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { NgxsModule, State, Action, Store, Actions } from '@ngxs/store';
+
+describe('https://github.com/ngxs/store/issues/1568', () => {
+  class MyAction {
+    static type = '[MyState] My action';
+  }
+
+  @State<string>({
+    name: 'myState',
+    defaults: 'STATE_VALUE'
+  })
+  class MyState {
+    @Action(MyAction)
+    handleAction(): Observable<string> {
+      return of('handleAction()');
+    }
+
+    @Action(MyAction)
+    handleAction1(): Observable<never> {
+      return of();
+    }
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([MyState])]
+    });
+  });
+
+  it('MyAction should not be canceled', () => {
+    // Arrange
+    const store = TestBed.inject(Store);
+    const actions$ = TestBed.inject(Actions);
+    const contexts: any[] = [];
+
+    // Act
+    const subscription = actions$.subscribe(ctx => {
+      contexts.push(ctx);
+    });
+
+    store.dispatch(new MyAction());
+
+    // Assert
+    expect(contexts).toEqual([
+      {
+        action: {},
+        status: 'DISPATCHED'
+      },
+      {
+        action: {},
+        status: 'SUCCESSFUL'
+      }
+    ]);
+
+    subscription.unsubscribe();
+  });
+});
