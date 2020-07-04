6539bd05e6c40b52efca1ba04e671b3aa720ef67
Joaquin Cid <joaqcid@gmail.com>
2020-07-04 18:44:52 -0300

    test: include e2e test

---
+++ integrations/hello-world-ng10-ivy/cypress/plugins/cypress-webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
