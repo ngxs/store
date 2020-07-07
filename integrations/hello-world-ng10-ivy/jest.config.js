aebff3782f8368ff6463462e2bfe4a233bc41a6d
Joaquin Cid <joaqcid@gmail.com>
2020-07-07 08:40:03 -0300

    test: add test

--- integrations/hello-world-ng10-ivy/jest.config.js
+++ integrations/hello-world-ng10-ivy/jest.config.js
@@ -3,5 +3,12 @@ const path = require('path');
 module.exports = {
   displayName: 'hello-world-ng10-ivy',
   rootDir: path.resolve(),
-  testMatch: ['<rootDir>/src/**/**/*.spec.ts']
+  testMatch: ['<rootDir>/src/**/**/*.spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      allowSyntheticDefaultImports: true
+    }
+  }
 };
