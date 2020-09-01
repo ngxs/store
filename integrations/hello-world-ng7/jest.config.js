4007ce4f9cef50e5929852b066caaf4dd7cf9556
apolo <agustin.polo@cloudx.com>
2020-09-01 20:41:02 -0300

    test: remove unit test run from integration

--- integrations/hello-world-ng7/jest.config.js
+++ integrations/hello-world-ng7/jest.config.js
@@ -3,5 +3,11 @@ const path = require('path');
 module.exports = {
   displayName: 'ngxs-ivy',
   rootDir: path.resolve(),
-  testMatch: ['<rootDir>/src/**/**/*.spec.ts']
+  testMatch: ['<rootDir>/src/**/**/*.spec.ts'],
+  astTransformers: {
+    before: [
+      'jest-preset-angular/build/InlineFilesTransformer',
+      'jest-preset-angular/build/StripStylesTransformer'
+    ]
+  }
 };
