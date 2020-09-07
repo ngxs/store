cc1e3c43f40145d016e82dc920b13d775697c5fb
apolo <agustin.polo@cloudx.com>
2020-09-07 13:51:03 -0300

    test: remove test because jest builder conflicts with angular 7

--- integrations/hello-world-ng7/jest.config.js
+++ integrations/hello-world-ng7/jest.config.js
@@ -3,11 +3,5 @@ const path = require('path');
 module.exports = {
   displayName: 'ngxs-ivy',
   rootDir: path.resolve(),
-  testMatch: ['<rootDir>/src/**/**/*.spec.ts'],
-  astTransformers: {
-    before: [
-      'jest-preset-angular/build/InlineFilesTransformer',
-      'jest-preset-angular/build/StripStylesTransformer'
-    ]
-  }
+  testMatch: ['<rootDir>/src/**/**/*.spec.ts']
 };
