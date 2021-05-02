dc42f97cae4d726d1ef56f988a1cb70dc96f4f9b
arturovt <arthurandrosovich@gmail.com>
2021-05-02 20:41:29 +0300

    test: add SSR tests with many concurrent requests

---
+++ integrations/hello-world-ng11-ivy/cypress/integration-ssr/issue-1646-wrong-ssr-state.spec.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Select decorator returning state from the wrong store during SSR (https://github.com/ngxs/store/issues/1646)', () => {
+  it('should make concurrent requests and app should render correctly for each request', async () => {
+    const promises: Promise<string>[] = Array.from({ length: 100 }).map(() =>
+      fetch('/').then(res => res.text())
+    );
+
+    const bodies = await Promise.all(promises);
+
+    bodies.forEach(body => {
+      expect(body).to.contain('Angular 11 (ivy) Integration Test');
+      expect(body).to.contain('Counter is 0');
+    });
+  });
+});
