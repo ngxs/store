cdd7ae38c6a2fcf813f3211c6b574622a96f0ed0
Artur Androsovych <arthurandrosovich@gmail.com>
2022-03-16 21:25:54 +0200

    fix(store): complete the state stream once the root view is removed (#1830)

--- packages/store/src/internal/state-stream.ts
+++ packages/store/src/internal/state-stream.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { PlainObject } from '@ngxs/store/internals';
@@ -8,8 +8,15 @@ import { PlainObject } from '@ngxs/store/internals';
  * @ignore
  */
 @Injectable()
-export class StateStream extends BehaviorSubject<PlainObject> {
+export class StateStream extends BehaviorSubject<PlainObject> implements OnDestroy {
   constructor() {
     super({});
+  }
+
+  ngOnDestroy(): void {
+    // The `StateStream` should never emit values once the root view is removed, e.g. when the `NgModuleRef.destroy()` is called.
+    // This will eliminate memory leaks in server-side rendered apps where the `StateStream` is created per each HTTP request, users
+    // might forget to unsubscribe from `store.select` or `store.subscribe`, thus this will lead to huge memory leaks in SSR apps.
+    this.complete();
   }
 }
