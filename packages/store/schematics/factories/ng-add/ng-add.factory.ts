d1606f99d71f4885ba71f88abc08b2dcf60f2a45
Vadim <zraw.stdio@gmail.com>
2022-05-22 00:54:55 +0300

    feat(ngadd): improve the ngxs ng-add schema and factory, add unit tests (#1838)
    * feat(ngadd): improve the ngxs ng-add schema and factory, add unit test
    
    - [x] update the lib.config - add more available ngxs plugins;
    - [x] ng-add schema - add selectable packages, add optional application project name;
    - [x] ng-add factory - add selectable ngxs packages if they are not present yet;
    - [x] ng-add factory - import the ngxs module in the root app module if project name is provided;
    - [x] implement a basic ng-add factory unit test;
    - [x] add the missing angular-devkit/schematics-cli package;
    - [x] add the package commands to run existing ngxs schematics;
    
    * Update packages/store/schematics/factories/ng-add/ng-add.factory.spec.ts
    
    - [x] add comments matching preferred test style;
    
    Co-authored-by: Mark Whitfeld <markwhitfeld@users.noreply.github.com>
    
    * Update packages/store/schematics/factories/ng-add/ng-add.factory.spec.ts
    
    - [x] add comments matching preferred test style;
    - [x] tweak module import to be more typically formatted;
    
    Co-authored-by: Mark Whitfeld <markwhitfeld@users.noreply.github.com>
    
    * Update packages/store/schematics/factories/ng-add/ng-add.factory.ts
    
    - [x] fix typos;
    
    Co-authored-by: Mark Whitfeld <markwhitfeld@users.noreply.github.com>
    
    * Update packages/store/schematics/factories/ng-add/ng-add.factory.ts
    
    - [x] format tweaks, so that prettier has less work to do;
    
    Co-authored-by: Mark Whitfeld <markwhitfeld@users.noreply.github.com>
    
    * refactor(schematics): add a shared library configuration options object
    
    Co-authored-by: Mark Whitfeld <markwhitfeld@users.noreply.github.com>

--- packages/store/schematics/factories/ng-add/ng-add.factory.ts
+++ packages/store/schematics/factories/ng-add/ng-add.factory.ts
@@ -1,29 +1,49 @@
-import { chain, noop, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  chain,
+  noop,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree
+} from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
   addPackageJsonDependency,
+  getPackageJsonDependency,
   NodeDependency
 } from '@schematics/angular/utility/dependencies';
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+import { getProject } from '@schematics/angular/utility/project';
+import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
 
-import { LIB_CONFIG } from '../../utils/common/lib.config';
+import { LIBRARIES, LIB_CONFIG } from '../../utils/common/lib.config';
 
 import { NgxsPackageSchema } from './ng-add.schema';
+import { InsertChange } from '@schematics/angular/utility/change';
+import * as ts from '@schematics/angular/node_modules/typescript';
 
 export function ngAdd(options: NgxsPackageSchema): Rule {
   return chain([
-    addNgxsPackageToPackageJson(),
-    // setSchematicsAsDefault(),
+    addNgxsPackageToPackageJson(options),
     finallyLog(),
-    options.skipInstall ? noop() : runNpmPackageInstall()
+    options.skipInstall ? noop() : runNpmPackageInstall(),
+    options.skipInstall ? noop() : addDeclarationToNgModule(options)
   ]);
 }
 
-function addNgxsPackageToPackageJson() {
+function addNgxsPackageToPackageJson(options: NgxsPackageSchema) {
   return (host: Tree, context: SchematicContext) => {
     context.logger.info('Adding npm dependencies');
-    LIB_CONFIG.forEach(({ type, version, name, overwrite }: NodeDependency) => {
-      addPackageJsonDependency(host, { type, version, name, overwrite });
-      context.logger.log('info', `✅️ Added "${name}" into ${type}`);
+    LIB_CONFIG.filter(
+      lib => lib.name === LIBRARIES.STORE || options.packages?.includes(lib.name)
+    ).forEach(({ type, version, name, overwrite }: NodeDependency) => {
+      const packageExists = getPackageJsonDependency(host, name);
+      if (packageExists === null) {
+        addPackageJsonDependency(host, { type, version, name, overwrite });
+        context.logger.info(`✅️ Added "${name}" into ${type}`);
+      } else {
+        context.logger.warn(`✅️ "${name}" already exists in the ${type}`);
+      }
     });
     return host;
   };
@@ -32,27 +52,65 @@ function addNgxsPackageToPackageJson() {
 function runNpmPackageInstall() {
   return (_: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
-    context.logger.log('info', `🔍 Installing packages...`);
+    context.logger.info(`🔍 Installing packages...`);
   };
 }
 
-// function setSchematicsAsDefault(): Rule {
-//   return (host: Tree, context: SchematicContext) => {
-//     context.logger.info('Adding @ngxs/schematics to angular.json');
-//     const exec = require('child_process').exec;
-//     exec('ng config cli.defaultCollection @ngxs/schematics', () => {
-//       context.logger.log('info', `✅️ Setting NGXS Schematics as default`);
-//     });
-//     return host;
-//   };
-// }
-
 function finallyLog(): Rule {
   return (host: Tree, context: SchematicContext) => {
-    context.logger.log(
-      'info',
+    context.logger.info(
       `👏 Create your first ngxs store by using starter kit: ng g ngxs-sk --spec`
     );
+
+    return host;
+  };
+}
+
+function addDeclarationToNgModule(options: NgxsPackageSchema): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    if (typeof options.name === 'undefined' || options.name === '') {
+      return host;
+    }
+
+    const project = getProject(host, options.name);
+
+    context.logger.info(JSON.stringify(project));
+
+    if (typeof project === 'undefined' || project === null) {
+      throw new SchematicsException(`Project "${options.name}" does not exist.`);
+    }
+
+    const modulePath = getAppModulePath(host, `${project.root}/src/main.ts`);
+
+    context.logger.info(`module path, ${modulePath}`);
+
+    if (typeof modulePath === 'undefined') {
+      throw new SchematicsException(
+        `Module path for project "${options.name}" does not exist.`
+      );
+    }
+
+    const importPath = '@ngxs/store';
+
+    const moduleImport =
+      'NgxsModule.forRoot([], { developmentMode: !environment.production, selectorOptions: { suppressErrors: false, injectContainerState: false } })';
+
+    const sourceBuffer = host.read(modulePath);
+
+    if (sourceBuffer !== null) {
+      const sourceText = sourceBuffer.toString();
+      const source = ts.createSourceFile(modulePath, sourceText, ts.ScriptTarget.Latest, true);
+
+      const changes = addImportToModule(source, modulePath, moduleImport, importPath);
+
+      const recorder = host.beginUpdate(modulePath);
+      for (const change of changes) {
+        if (change instanceof InsertChange) {
+          recorder.insertLeft(change.pos, change.toAdd);
+        }
+      }
+      host.commitUpdate(recorder);
+    }
 
     return host;
   };
--- packages/store/schematics/utils/common/lib.config.ts
+++ packages/store/schematics/utils/common/lib.config.ts
@@ -1,28 +1,63 @@
 import { NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
 
 export enum LIBRARIES {
-  STORE = '@ngxs/store',
+  DEVTOOLS = '@ngxs/devtools-plugin',
+  FORM = '@ngxs/form-plugin',
+  HMR = '@ngxs/hmr-plugin',
   LOGGER = '@ngxs/logger-plugin',
-  DEVTOOLS = '@ngxs/devtools-plugin'
+  ROUTER = '@ngxs/router-plugin',
+  STORAGE = '@ngxs/storage-plugin',
+  STORE = '@ngxs/store',
+  WEBSOCKET = '@ngxs/websocket-plugin'
 }
+
+/**
+ * Common library configuration options.
+ */
+const configOptions = {
+  version: '3.7.3',
+  overwrite: true
+};
 
 export const LIB_CONFIG: NodeDependency[] = [
   {
     type: NodeDependencyType.Default,
     name: LIBRARIES.DEVTOOLS,
-    version: '^3.3.2',
-    overwrite: true
+    ...configOptions
+  },
+  {
+    type: NodeDependencyType.Default,
+    name: LIBRARIES.FORM,
+    ...configOptions
+  },
+  {
+    type: NodeDependencyType.Default,
+    name: LIBRARIES.HMR,
+    ...configOptions
   },
   {
     type: NodeDependencyType.Default,
     name: LIBRARIES.LOGGER,
-    version: '^3.3.2',
-    overwrite: true
+    ...configOptions
+  },
+  {
+    type: NodeDependencyType.Default,
+    name: LIBRARIES.ROUTER,
+    ...configOptions
+  },
+  {
+    type: NodeDependencyType.Default,
+    name: LIBRARIES.STORAGE,
+    ...configOptions
   },
   {
     type: NodeDependencyType.Default,
     name: LIBRARIES.STORE,
-    version: '^3.3.2',
-    overwrite: true
+    ...configOptions
+  },
+  {
+    type: NodeDependencyType.Default,
+    name: LIBRARIES.WEBSOCKET,
+    ...configOptions
   }
 ];
