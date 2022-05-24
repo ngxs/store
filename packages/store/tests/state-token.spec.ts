121c6096f1272cf990f8bebeb83ab34256201b09
rfprod <zoidenmacht@zoho.com>
2022-05-25 01:04:14 +0300

    test(unit): rewrite the state token spec

--- packages/store/tests/state-token.spec.ts
+++ packages/store/tests/state-token.spec.ts
@@ -1,5 +1,5 @@
 import { NgxsModule, Select, Selector, State, StateToken, Store } from '@ngxs/store';
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, Injectable } from '@angular/core';
 import { take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -15,45 +15,51 @@ describe('[TEST]: StateToken', () => {
   });
 
   describe('Integration', () => {
-    it('should successfully create store', async(() => {
-      const TODO_LIST_TOKEN = new StateToken<string[]>('todoList');
+    const TODO_LIST_TOKEN = new StateToken<string[]>('todoList');
 
-      @State<string[]>({
-        name: TODO_LIST_TOKEN,
-        defaults: ['hello', 'world']
-      })
-      @Injectable()
-      class TodoListState {
-        @Selector([TODO_LIST_TOKEN])
-        static todos(state: string[]) {
-          return state;
-        }
-
-        @Selector([TODO_LIST_TOKEN])
-        static first(state: string[]): string {
-          return state.slice().shift()!;
-        }
+    @State<string[]>({
+      name: TODO_LIST_TOKEN,
+      defaults: ['hello', 'world']
+    })
+    @Injectable()
+    class TodoListState {
+      @Selector([TODO_LIST_TOKEN])
+      static todos(state: string[]) {
+        return state;
       }
 
-      @Component({
-        selector: 'myApp',
-        template: '{{ myState$ | async | json }}'
-      })
-      class MyComponent {
-        @Select(TODO_LIST_TOKEN)
-        myState$: Observable<string[]>;
-
-        constructor(public storeApp: Store) {}
+      @Selector([TODO_LIST_TOKEN])
+      static first(state: string[]): string {
+        return state.slice().shift()!;
       }
+    }
 
-      TestBed.configureTestingModule({
+    @Component({
+      selector: 'myApp',
+      template: '{{ myState$ | async | json }}'
+    })
+    class MyComponent {
+      @Select(TODO_LIST_TOKEN)
+      myState$: Observable<string[]>;
+
+      constructor(public storeApp: Store) {}
+    }
+
+    let fixture: ComponentFixture<MyComponent>;
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
         imports: [NgxsModule.forRoot([TodoListState])],
         declarations: [MyComponent]
-      });
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(MyComponent);
+          fixture.autoDetectChanges();
+        });
+    });
 
-      const fixture = TestBed.createComponent(MyComponent);
-      fixture.autoDetectChanges();
-
+    it('should successfully create store', async () => {
       const store: Store = fixture.componentInstance.storeApp;
       expect(store.selectSnapshot(TodoListState.todos)).toEqual(['hello', 'world']);
       expect(store.selectSnapshot(TodoListState.first)).toEqual('hello');
@@ -64,18 +70,14 @@ describe('[TEST]: StateToken', () => {
 
       expect(store.selectSnapshot(TODO_LIST_TOKEN)).toEqual(['hello', 'world']);
 
-      const selectResult: string[] = [];
-      store
+      const selectResult: string[] = await store
         .select(TODO_LIST_TOKEN)
         .pipe(take(1))
-        .subscribe((value: string[]) => selectResult.push(...value));
+        .toPromise();
       expect(selectResult).toEqual(['hello', 'world']);
 
-      const selectOnceResult: string[] = [];
-      store
-        .selectOnce(TODO_LIST_TOKEN)
-        .subscribe((value: string[]) => selectOnceResult.push(...value));
+      const selectOnceResult: string[] = await store.selectOnce(TODO_LIST_TOKEN).toPromise();
       expect(selectOnceResult).toEqual(['hello', 'world']);
-    }));
+    });
   });
 });
