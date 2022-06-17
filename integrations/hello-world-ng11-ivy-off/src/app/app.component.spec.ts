6d8cd8a0ce3882a1d02996237699732c899be82c
rfprod <zoidenmacht@zoho.com>
2022-06-17 20:20:56 +0300

    test(integration): fix integration test ng11-ivy-off

--- integrations/hello-world-ng11-ivy-off/src/app/app.component.spec.ts
+++ integrations/hello-world-ng11-ivy-off/src/app/app.component.spec.ts
@@ -1,26 +1,32 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store';
 
 describe('AppComponent', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [RouterTestingModule, StoreModule],
-        declarations: [AppComponent]
-      }).compileComponents();
-    })
-  );
+  const setup = async () => {
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    return TestBed.configureTestingModule({
+      imports: [RouterTestingModule, StoreModule],
+      declarations: [AppComponent]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        return { fixture, component };
+      });
+  };
+
+  it('should create the app', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render title', async () => {
+    const { fixture } = await setup();
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
