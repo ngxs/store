3842e75b4fff5f98b01022d44e85222a42588726
Oleksandr Saulyak <oleksandr.saulyak@codefresh.io>
2022-07-21 15:25:10 +0300

    fixed eslint issues

--- packages/devtools-plugin/src/symbols.ts
+++ packages/devtools-plugin/src/symbols.ts
@@ -34,23 +34,30 @@ export interface NgxsDevtoolsOptions {
   maxAge?: number;
 
   /**
-   * If more than one action is dispatched in the indicated interval, all new actions will be collected and sent at once. It is the joint between performance and speed. When set to 0, all actions will be sent instantly. Set it to a higher value when experiencing perf issues (also maxAge to a lower value). Default is 500 ms.
+   * If more than one action is dispatched in the indicated interval, all new actions will be collected
+   * and sent at once. It is the joint between performance and speed. When set to 0, all actions will be
+   * sent instantly. Set it to a higher value when experiencing perf issues (also maxAge to a lower value).
+   * Default is 500 ms.
    */
   latency?: number;
 
   /**
-   * string or array of strings as regex - actions types to be hidden in the monitors (while passed to the reducers). If actionsWhitelist specified, actionsBlacklist is ignored.
+   * string or array of strings as regex - actions types to be hidden in the monitors (while passed to the reducers).
+   * If actionsWhitelist specified, actionsBlacklist is ignored.
    */
   actionsBlacklist?: string | string[];
 
   /**
-   * string or array of strings as regex - actions types to be shown in the monitors (while passed to the reducers). If actionsWhitelist specified, actionsBlacklist is ignored.
+   * string or array of strings as regex - actions types to be shown in the monitors (while passed to the reducers).
+   * If actionsWhitelist specified, actionsBlacklist is ignored.
    */
   actionsWhitelist?: string | string[];
 
 
   /**
-   * called for every action before sending, takes state and action object, and returns true in case it allows sending the current data to the monitor. Use it as a more advanced version of actionsBlacklist/actionsWhitelist parameters
+   * called for every action before sending, takes state and action object, and returns true in case it allows
+   * sending the current data to the monitor. Use it as a more advanced version of
+   * actionsBlacklist/actionsWhitelist parameters
    */
   predicate?: (state: any, action: any) => boolean;
 
