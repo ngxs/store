caa9f14b244ecaa70a2ffcc126c3cfe86b17a64c
markwhitfeld <markwhitfeld@users.noreply.github.com>
2023-03-20 13:27:02 +0200

    test: add tests for all createSelector signatures

--- packages/store/types/tests/create-selector.lint.ts
+++ packages/store/types/tests/create-selector.lint.ts
@@ -11,7 +11,7 @@ describe('[TEST]: createSelector', () => {
 
   beforeAll(() => {
     TestBed.configureTestingModule({
-      imports: [NgxsModule.forRoot()]
+      imports: [NgxsModule.forRoot()],
     });
 
     store = TestBed.get(Store);
@@ -20,14 +20,13 @@ describe('[TEST]: createSelector', () => {
   it('should be correct detect type with createSelector', () => {
     @State<string[]>({
       name: 'todo',
-      defaults: []
+      defaults: [],
     })
     class TodoState {
       // $ExpectType () => (state: string[]) => string[]
       static todoA() {
-        return createSelector(
-          [TodoState],
-          (state: string[]) => state.map(todo => todo).reverse()
+        return createSelector([TodoState], (state: string[]) =>
+          state.map((todo) => todo).reverse()
         );
       }
 
@@ -52,7 +51,7 @@ describe('[TEST]: createSelector', () => {
           {
             containerClass: Infinity,
             selectorName: 'Hello world',
-            getSelectorOptions: () => ({})
+            getSelectorOptions: () => ({}),
           }
         );
       }
@@ -76,8 +75,8 @@ describe('[TEST]: createSelector', () => {
     @State<TestStateModel>({
       name: 'state',
       defaults: {
-        prop: null!
-      }
+        prop: null!,
+      },
     })
     class TestState {
       @Selector()
@@ -95,14 +94,14 @@ describe('[TEST]: createSelector', () => {
 
     const selectStateModelProp = createSelector(
       [selectTestStateModel],
-      model => model.prop // $ExpectType (model: TestStateModel) => string
+      (model) => model.prop // $ExpectType (model: TestStateModel) => string
     );
 
     assertType(() => selectStateModelProp); // $ExpectType (model: TestStateModel) => string
 
     const selectStateModelPropBySelector = createSelector(
       [TestState.foo],
-      state => state // $ExpectType (state: string) => string
+      (state) => state // $ExpectType (state: string) => string
     );
 
     assertType(() => selectStateModelPropBySelector); // $ExpectType (state: string) => string
@@ -120,5 +119,30 @@ describe('[TEST]: createSelector', () => {
     );
 
     assertType(() => mixinSelector); // $ExpectType (a: TestStateModel, b: string, c: { state: any; someOtherData: string; }) => { a: TestStateModel; b: string; c: { state: any; someOtherData: string; }; }
+  });
+
+  it('should infer projector parameter types from selectors', () => {
+    const val = 'x' as const;
+    const a = () => ['a'] as const;
+    const b = () => ['b'] as const;
+    const c = () => ['c'] as const;
+    const d = () => ['d'] as const;
+    const e = () => ['e'] as const;
+    const f = () => ['f'] as const;
+    const g = () => ['g'] as const;
+    const h = () => ['h'] as const;
+
+    function assertReturnType<T extends (...args: any) => any>(fn: T): ReturnType<T> {
+      return null as unknown as ReturnType<T>;
+    }
+
+    assertReturnType(createSelector([a],(a) => [...a] as const)); // $ExpectType readonly ["a"]
+    assertReturnType(createSelector([a, b],(a, b) => [...a, ...b] as const)); // $ExpectType readonly ["a", "b"]
+    assertReturnType(createSelector([a, b, c],(a, b, c) => [...a, ...b, ...c] as const)); // $ExpectType readonly ["a", "b", "c"]
+    assertReturnType(createSelector([a, b, c, d],(a, b, c, d) => [...a, ...b, ...c, ...d] as const )); // $ExpectType readonly ["a", "b", "c", "d"]
+    assertReturnType(createSelector([a, b, c, d, e],(a, b, c, d, e) => [...a, ...b, ...c, ...d, ...e] as const)); // $ExpectType readonly ["a", "b", "c", "d", "e"]
+    assertReturnType(createSelector([a, b, c, d, e, f],(a, b, c, d, e, f) => [...a, ...b, ...c, ...d, ...e, ...f] as const)); // $ExpectType readonly ["a", "b", "c", "d", "e", "f"]
+    assertReturnType(createSelector([a, b, c, d, e, f, g],(a, b, c, d, e, f, g) => [...a, ...b, ...c, ...d, ...e, ...f, ...g] as const)); // $ExpectType readonly ["a", "b", "c", "d", "e", "f", "g"]
+    assertReturnType(createSelector([a, b, c, d, e, f, g, h],(a, b, c, d, e, f, g, h) => [...a, ...b, ...c, ...d, ...e, ...f, ...g, ...h] as const)); // $ExpectType readonly ["a", "b", "c", "d", "e", "f", "g", "h"]
   });
 });
