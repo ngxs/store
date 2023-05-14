da8b6c1ca06cad0a01623e2296ac32c47c0b356c
arturovt <arthurandrosovich@gmail.com>
2023-05-14 22:04:25 +0300

    build: upgrade cypress

--- integrations/hello-world-ng16/src/app/app.component.spec.ts
+++ integrations/hello-world-ng16/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store';
