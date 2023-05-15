396fa67861c80a9660390b94b45852f088218fda
arturovt <arthurandrosovich@gmail.com>
2023-05-15 10:45:31 +0300

    chore: run Cypress migration in workspace root

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  responseTimeout: 60000,
+  pageLoadTimeout: 120000,
+  e2e: {
+    supportFile: false,
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:4200',
+    excludeSpecPattern: ['**/plugins/**.js', '**/tsconfig.json']
+  }
+});
