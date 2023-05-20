d21d91a8a3fae4d8d10bfc31d3e4cee9ac823044
arturovt <arthurandrosovich@gmail.com>
2023-05-20 22:59:04 +0300

    refactor(store): remove `isAngularInTestMode`

--- packages/store/internals/src/index.ts
+++ packages/store/internals/src/index.ts
@@ -1,4 +1,3 @@
-export { isAngularInTestMode } from './angular';
 export { NgxsBootstrapper } from './ngxs-bootstrapper';
 export { memoize } from './memoize';
 export { INITIAL_STATE_TOKEN, InitialState } from './initial-state';
