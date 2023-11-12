3300af7d916554705339cd8ec8b803e46c84eb60
Mark Whitfeld <markwhitfeld@users.noreply.github.com>
2023-11-12 23:12:19 +0200

    fix: exclude folder from jest coverage report

--- packages/store/jest.config.js
+++ packages/store/jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
     }
   },
   coverageDirectory: '../../coverage/packages/store',
+  coveragePathIgnorePatterns: [
+    "<rootDir>/schematics/src/utils/ng-utils/**/*.ts"
+  ]
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular'
   },
