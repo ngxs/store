221babd48a31c9e82748c144df79e1371fb5e020
markwhitfeld <markwhitfeld@users.noreply.github.com>
2024-03-22 01:29:23 +0200

    fix: issue with Injector destroyed

--- packages/store/src/ngxs-error-handler.ts
+++ packages/store/src/ngxs-error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
+import { ErrorHandler, Inject, Injectable, NgZone, forwardRef } from '@angular/core';
 
 export interface NgxsErrorContext {
   action?: any;
@@ -6,16 +6,12 @@ export interface NgxsErrorContext {
 
 @Injectable({ providedIn: 'root' })
 export class NgxsUnhandledErrorHandler {
-  private _errorHandler: ErrorHandler = null!;
-
   constructor(
     private _ngZone: NgZone,
-    private _injector: Injector
+    @Inject(forwardRef(() => ErrorHandler)) private _errorHandler: ErrorHandler
   ) {}
 
   handleError(_error: any, _context: NgxsErrorContext): void {
-    // Retrieve lazily to avoid cyclic dependency exception.
-    this._errorHandler ||= this._injector.get(ErrorHandler);
     // In order to avoid duplicate error handling, it is necessary to leave
     // the Angular zone to ensure that errors are not caught twice. The `handleError`
     // method may contain a `throw error` statement, which is used to re-throw the error.
