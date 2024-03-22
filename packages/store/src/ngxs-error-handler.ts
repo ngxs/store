00e1bb75ed3728b9fa362939b0a5e0f4bfd535fd
markwhitfeld <markwhitfeld@users.noreply.github.com>
2024-03-22 02:12:44 +0200

    fix: must lazy inject ErrorHandler

--- packages/store/src/ngxs-error-handler.ts
+++ packages/store/src/ngxs-error-handler.ts
@@ -1,17 +1,26 @@
-import { ErrorHandler, Inject, Injectable, NgZone, forwardRef } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone, OnInit } from '@angular/core';
 
 export interface NgxsErrorContext {
   action?: any;
 }
 
 @Injectable({ providedIn: 'root' })
-export class NgxsUnhandledErrorHandler {
+export class NgxsUnhandledErrorHandler implements OnInit {
+  private _errorHandler: ErrorHandler = null!;
+
   constructor(
     private _ngZone: NgZone,
-    @Inject(forwardRef(() => ErrorHandler)) private _errorHandler: ErrorHandler
+    private _injector: Injector
   ) {}
 
+  ngOnInit(): void {
+    // Retrieve lazily to avoid cyclic dependency exception.
+    this._errorHandler ||= this._injector.get(ErrorHandler);
+  }
+
   handleError(_error: any, _context: NgxsErrorContext): void {
+    // Retrieve lazily to avoid cyclic dependency exception.
+    this._errorHandler ||= this._injector.get(ErrorHandler);
     // In order to avoid duplicate error handling, it is necessary to leave
     // the Angular zone to ensure that errors are not caught twice. The `handleError`
     // method may contain a `throw error` statement, which is used to re-throw the error.
