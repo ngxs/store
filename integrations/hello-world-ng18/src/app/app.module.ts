f410b9d37fd0e1e8e1e4b28f92a5c9a059eacb52
markwhitfeld <markwhitfeld@users.noreply.github.com>
2024-05-23 13:23:23 +0200

    chore: copy ng18 project

---
+++ integrations/hello-world-ng18/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
+
+const routes: Routes = [{ path: '', component: AppComponent }];
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'hello-world-ng17' }),
+    RouterModule.forRoot(routes),
+    StoreModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
