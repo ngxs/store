81793e5d2f995e21665451abe402bec3dfb2be72
markwhitfeld <markwhitfeld@users.noreply.github.com>
2024-08-21 22:43:20 +0200

    chore: fix zone detection

--- packages/store/src/selectors/selector-checks.util.ts
+++ packages/store/src/selectors/selector-checks.util.ts
@@ -1,5 +1,6 @@
 import { ɵgetSelectorMetadata, ɵgetStoreMetadata } from '@ngxs/store/internals';
 import { ɵSelectorDef } from './selector-types.util';
+import { NgZone } from '@angular/core';
 
 function getMissingMetaDataError(
   selector: ɵSelectorDef<any>,
@@ -26,18 +27,20 @@ export function ensureValidSelector(
     // If we have used this utility within a state class, we may be
     //  before the @State or @Selector decorators have been applied.
     //  wait until the next microtask to verify.
-const isNgZoneEnabled = typeof Zone !== 'undefined';
-const runOutsideAngular = isNgZoneEnabled ? (fn) => Zone.root.run(fn) : fn => fn();
-
-    runOutsideAngular(() => {
-      Promise.resolve().then(() => {   
-      const errorAgain = getMissingMetaDataError(selector, { noun, prefix });
-      if (errorAgain) {
-        // Throw the originally captured error so that the stack trace shows the
-        // original utility call site.
-        console.error(error);
-      }
-    });
+    // Theoretically this situation is only encountered when the javascript
+    //  files are being loaded and we are outside the angular zone.
+    if (!NgZone.isInAngularZone()) {
+      Promise.resolve().then(() => {
+        const errorAgain = getMissingMetaDataError(selector, { noun, prefix });
+        if (errorAgain) {
+          // Throw the originally captured error so that the stack trace shows the
+          // original utility call site.
+          console.error(error);
+        }
+      });
+    } else {
+      throw error;
+    }
   }
 }
 
