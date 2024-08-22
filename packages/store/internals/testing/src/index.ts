77cf50dfaab54b3f6e9431876e1d06ac49cbb2e3
Mark Whitfeld <markwhitfeld@users.noreply.github.com>
2024-08-22 17:28:43 +0200

    fix: allow selector utils usage within state class (#2210)

--- packages/store/internals/testing/src/index.ts
+++ packages/store/internals/testing/src/index.ts
@@ -1,4 +1,9 @@
 export { freshPlatform } from './fresh-platform';
 export { NgxsTestBed } from './ngxs.setup';
-export { skipConsoleLogging } from './skip-console-logging';
+export {
+  skipConsoleLogging,
+  ConsoleRecord,
+  ConsoleRecorder,
+  loggedError
+} from './skip-console-logging';
 export { NgxsTesting } from './symbol';
