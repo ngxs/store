40be833aa6610941be997006278676350c9ab3f5
Moritz Pietzschke <git@mopie.de>
2024-09-23 12:09:12 +0200

    fix(store): disallow nullable actions in `dispatch()`
    In this commit, we update the `dispatch()` implementation to account for nullable parameters.
    For example, `dispatch(null)` is invalid because `null` is not a valid action. Changing the type
    from `any` to `NonNullable<unknown>` is a breaking change. To make this transition more
    user-friendly, we include a check, guarded by `ngDevMode`, that throws an error immediately
    if `actions` is `null` or `undefined`. This change will only trigger runtime errors in
    development mode, ensuring that no production applications are affected.

---
+++ packages/store/tests/issues/dispatch-with-nullable-arguments.spec.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Action, provideStore, State, Store } from '@ngxs/store';
+
+describe('dispatch() with nullable arguments', () => {
+  class MyAction {
+    static type = '[MyState] My action';
+  }
+
+  @State<string>({
+    name: 'myState',
+    defaults: 'STATE_VALUE'
+  })
+  @Injectable()
+  class MyState {
+    @Action(MyAction)
+    handleAction() {
+      // Empty.
+    }
+  }
+
+  const testSetup = () => {
+    TestBed.configureTestingModule({
+      providers: [provideStore([MyState])]
+    });
+
+    return TestBed.inject(Store);
+  };
+
+  it('should throw an error when `dispatch()` is called with nullable arguments', () => {
+    // Arrange
+    const recordedErrors: Error[] = [];
+    const args = [null, undefined, [new MyAction(), null], [new MyAction(), undefined]];
+    const store = testSetup();
+
+    // Act
+    args.forEach(arg =>
+      store.dispatch(arg).subscribe({
+        error: error => recordedErrors.push(error)
+      })
+    );
+
+    // Assert
+    expect(recordedErrors.length).toEqual(4);
+    recordedErrors.forEach(error =>
+      expect(error.message).toEqual('`dispatch()` was called without providing an action.')
+    );
+  });
+});
