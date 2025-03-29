5bb8fb6c39acbf987cd8f9978b5107d5d2bed977
arturovt <arthurandrosovich@gmail.com>
2025-03-29 17:42:33 +0200

    refactor(store): inline `NGXS_EXECUTION_STRATEGY` factory

--- packages/store/src/execution/symbols.ts
+++ packages/store/src/execution/symbols.ts
@@ -3,18 +3,16 @@ import { InjectionToken, inject } from '@angular/core';
 import { NGXS_OPTIONS } from '../symbols';
 
 /**
- * The injection token is used internally to resolve an instance of the execution
- * strategy. It checks whether consumers have provided their own `executionStrategy`
- * and also verifies if we are operating in a zone-aware environment.
+ * The injection token is used internally to resolve an instance of the execution strategy.
  */
 export const NGXS_EXECUTION_STRATEGY = new InjectionToken<NgxsExecutionStrategy>(
   typeof ngDevMode !== 'undefined' && ngDevMode ? 'NGXS_EXECUTION_STRATEGY' : '',
   {
     providedIn: 'root',
-    factory: () => {
-      const options = inject(NGXS_OPTIONS);
-      return inject(options.executionStrategy!);
-    }
+    // Since `executionStrategy` is a `Type<...>`, we should inject it to retrieve an
+    // instance. This injection token essentially holds an instance of the
+    // execution strategy class.
+    factory: () => inject(inject(NGXS_OPTIONS).executionStrategy)
   }
 );
 
