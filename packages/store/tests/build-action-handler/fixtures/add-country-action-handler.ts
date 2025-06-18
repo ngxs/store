3984e88e6006d14703abe12cee7bc699b01247e5
arturovt <arthurandrosovich@gmail.com>
2025-06-18 17:57:09 +0300

    fix(store): add missing `detach` to action director
    That change has been lost in commits in the `ActionDirector` PR.

--- packages/store/tests/build-action-handler/fixtures/add-country-action-handler.ts
+++ packages/store/tests/build-action-handler/fixtures/add-country-action-handler.ts
@@ -4,7 +4,7 @@ import { COUNTRIES_STATE_TOKEN } from './state';
 import { AddCountry } from './actions';
 
 export const addCountryActionHandler = (actionDirector: ActionDirector) => {
-  actionDirector.attachAction(COUNTRIES_STATE_TOKEN, AddCountry, (ctx, action) => {
+  return actionDirector.attachAction(COUNTRIES_STATE_TOKEN, AddCountry, (ctx, action) => {
     ctx.setState(countries => [...countries, action.country]);
   });
 };
