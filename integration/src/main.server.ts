c08b592fdb444288ca820332d55bdb20bd8e80d9
arturovt <arthurandrosovich@gmail.com>
2025-10-01 10:48:13 +0300

    build: upgrade Nx workspace

--- integration/src/main.server.ts
+++ integration/src/main.server.ts
@@ -1,8 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appServerConfig } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, appServerConfig);
+const bootstrap = (context: BootstrapContext) =>
+  bootstrapApplication(AppComponent, appServerConfig, context);
 
 export default bootstrap;
